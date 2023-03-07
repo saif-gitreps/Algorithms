2716240055fe2f49658c3d4242389825d3564b27
saif-gitreps <110230611+saif-gitreps@users.noreply.github.com>
2023-03-07 22:13:13 +0600

    Update diagSumLinearly.cpp

--- diagSumLinearly.cpp
+++ diagSumLinearly.cpp
@@ -17,3 +17,21 @@ public:
         return sum;
     }
 };
+
+class Solution2 {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+    int sum=0;
+    for(int i=0;i<mat.size();i++)
+    {
+        for(int j=0;j<mat.size();j++)
+        {
+            if(i==j || ((i+j)==mat.size()-1))
+            {
+                sum+=mat[i][j];
+            }
+        }
+    }
+    return sum;
+    }
+};
