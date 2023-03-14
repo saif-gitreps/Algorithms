1ecaf3564d803ce61df370cb10b6604339d9f06d
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-14 12:01:31 +0600

    Update ZigZagLeetCode.cpp

--- Implementations/ZigZagLeetCode.cpp
+++ Implementations/ZigZagLeetCode.cpp
@@ -37,3 +37,33 @@ public:
         return ans;
     }
 };
+
+class Solution2 {
+public:
+    string convert(string s, int numRows) {
+        if (numRows <= 1) {
+            return s;
+        }
+        std::vector<std::string> list(numRows, "");
+        int currentLine = 0;
+        bool reachedEdge = true;
+
+        for (int i = 0; i < s.length(); ++i) {
+            if (currentLine == 0 || currentLine == numRows - 1) {
+                reachedEdge = !reachedEdge;
+            }
+            list.at(currentLine) += s.at(i);
+            if (!reachedEdge) {
+                currentLine++;
+            } else {
+                currentLine--;
+            }
+        }
+
+        std::string result;
+        for (int i = 0; i < numRows; ++i) {
+            result += list.at(i);
+        }
+        return result;
+    }
+};
