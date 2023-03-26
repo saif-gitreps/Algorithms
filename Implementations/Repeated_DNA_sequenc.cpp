9a601d02fa6ee35ac14f70772760b04a4a12d2f3
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-26 15:09:15 +0600

    Update Repeated_DNA_sequenc.cpp

--- Implementations/Repeated_DNA_sequenc.cpp
+++ Implementations/Repeated_DNA_sequenc.cpp
@@ -4,11 +4,11 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         map<string,int> m;
         vector<string> a;
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i+9<s.size();i++){
             m[s.substr(i,10)]++;
             //debug(s.substr(i,10));
         }
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i+9<s.size();i++){
          string sub = s.substr(i,10); 
             if(m[sub]>1){
                a.push_back(sub);
@@ -16,4 +16,34 @@ public:
             }
         }
     return a;}
+};
+/*
+although this is a java code , this beat 100%.
+public List<String> findRepeatedDnaSequences(String s) {
+    Set seen = new HashSet(), repeated = new HashSet();
+    for (int i = 0; i + 9 < s.length(); i++) {
+        String ten = s.substring(i, i + 10);
+        if (!seen.add(ten))
+            repeated.add(ten);
+    }
+    return new ArrayList(repeated);
+}
+
+*/
+
+class Solution2 {
+public:
+  // my optimized solution , slightly better , like 6% better.
+    vector<string> findRepeatedDnaSequences(string s) {
+        map<string,int> m;
+        vector<string> a;
+        for(int i=0;i+9<s.size();i++){
+            string sub = s.substr(i,10); 
+            m[sub]++;
+            if(m[sub]>1){
+               a.push_back(sub);
+               m[sub] = -1000000;
+            }
+        }
+        return a;}
 };
