64c0b489f97a1e7538b70d47623ff466916bf274
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-28 00:30:53 +0600

    Update plus_one.cpp

--- MathAlg/plus_one.cpp
+++ MathAlg/plus_one.cpp
@@ -20,3 +20,23 @@ public:
         }
     return a;}
 };
+
+class Solution {
+public:
+    // logically better solution.
+    vector<int> plusOne(vector<int>& a) {
+        int n = a.size();
+        for(int i = n-1; i>=0; i-- ){
+            if(a[i]==9){
+                a[i] = 0;
+            }
+            else{
+                a[i]++;
+                return a;
+            }
+        }
+        a.push_back(0);
+        a[0] = 1;
+        return a;
+        }
+};
