77e4ea62ff45e5053fd7c43bcc61f80eae483e1f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-03-29 23:08:51 +0600

    Update Shuffle_an_array.cpp

--- Implementations/Shuffle_an_array.cpp
+++ Implementations/Shuffle_an_array.cpp
@@ -33,3 +33,37 @@ private:
  */
 
 //=========================================================================//
+// so all of my logic was correct, except the shuffle aglorithm , so we had to use Fisher-Yate randomiser algorithm
+// it randomises/shuffles the array .
+
+class Solution {
+public:
+
+    Solution(vector<int>& num) {
+        this->nums = num;
+        copy = num;
+    }
+    vector<int> reset() {
+        this->nums = copy;
+        return copy;
+    }
+    
+    vector<int> shuffle() {
+        int n = this->nums.size();
+        for (int i = n-1; i >= 1; --i) {
+            int j = rand() % (i+1);
+            swap(this->nums[i],this->nums[j]);
+        }
+        return this->nums;
+    }
+private:
+    vector<int> nums;
+    vector<int> copy;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
