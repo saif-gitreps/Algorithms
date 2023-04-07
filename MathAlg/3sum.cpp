6fc5692bd43fa835789373aa3099944bf63b521b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-07 23:08:27 +0600

    Update 3sum.cpp

--- MathAlg/3sum.cpp
+++ MathAlg/3sum.cpp
@@ -24,3 +24,33 @@ public:
         
     }
 };
+
+//first attempt :1 
+// mistake was i forgot to add j++;k-- after the condition was fullfilled.
+// mistake two , had to start j at i+1 instead of 0 per loop.
+// mistake three, had to keep j<k becaue we dont want the same variable number.
+vector<vector<int>> threeSum(vector<int>& nums){
+   set<vector<int>> a;
+   vector<vector<int>> newarr;
+   sort(nums.begin(),nums.end());
+   int i=0,j=0,k=0;
+   while(i<nums.size()){
+      j=0; k = nums.size()-1;
+      while(j<=k){
+         if(nums[i]+nums[j]+nums[k]==0){
+            a.insert({nums[i],nums[j],nums[k]});
+         }
+         else if(nums[i]+nums[j]+nums[k]<0){
+            j++;
+         }
+         else{
+            k--;
+         }
+      }
+      i++;
+   }
+   for(auto s: a){
+      newarr.push_back(s);
+   }
+   return newarr;
+}
