49a650e30af0d6d51ea147ddad368e18de95cc0b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-09 01:08:07 +0600

    Update Valid_Anagram.cpp

--- Implementations/Valid_Anagram.cpp
+++ Implementations/Valid_Anagram.cpp
@@ -8,3 +8,23 @@ public:
     }
 };
 
+class Solution {
+public:
+  //best solution.
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        vector<int> v(26,0);
+        for(int i=0;i<s.size();i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(v[i]!=0)
+            return false;
+        }
+        return true;
+
+    }
+};
