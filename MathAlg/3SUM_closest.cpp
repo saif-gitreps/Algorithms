b41203fd8c84297fd0ad86fb957402c18a891499
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-08 21:54:20 +0600

    Update 3SUM_closest.cpp

--- MathAlg/3SUM_closest.cpp
+++ MathAlg/3SUM_closest.cpp
@@ -68,3 +68,45 @@ public:
       return (abs(mn-target)<abs(mx-target))? mn:mx;
     }
 };
+
+class Solution3 {
+  // very slow solution but it worked.
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+       vector<int> a;
+       set<int> s;
+       int i=0,j=0,k=0;
+       sort(nums.begin(),nums.end());
+       while(i<nums.size()){
+          j= i+1; k = nums.size()-1;
+          while(j<k){
+            s.insert(nums[i]+nums[j]+nums[k]);
+             if((nums[i]+nums[j]+nums[k])<target){
+               j++;
+             }
+             else if((nums[i]+nums[j]+nums[k])>target){
+               k--;
+             }
+             else{
+                j++;
+                k--;
+             }
+               
+          }
+          i++;
+       }
+       int mn=-1001,mx=1001;
+       for(auto num: s){
+          if(s.size()==1  || num==target){
+             return num;
+          }
+          if(num<target){
+             mn = max(num,mn);
+          }
+          else if(num>target){
+             mx = min(num,mx);
+          }
+       }
+      return (abs(mn-target)<abs(mx-target))? mn:mx;
+    }
+};
