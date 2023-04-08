4e5eeba76e65cb9d9c497fdbcd94e5bb9f205f99
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-09 00:54:24 +0600

    Create Valid_Anagram.cpp

---
+++ Implementations/Valid_Anagram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  // O(nlogn) soln.
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+    }
+};
+
