30d5b16ae60e7d62a703eec2d827476136e4538a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-20 13:07:06 +0600

    Update Single_number.cpp

--- Implementations/Single_number.cpp
+++ Implementations/Single_number.cpp
@@ -7,3 +7,16 @@ public:
 	   return ans;
     }
 }
+class Solution2 {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int count = 0;
+        for(int i = 1 ;i < nums.size() ; i += 2){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        return nums[nums.size()-1];
+    }
+};
