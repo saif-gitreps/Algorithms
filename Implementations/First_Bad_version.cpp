d19654a95a692b3372b58b7177cf75a912100ffe
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-29 16:18:47 +0600

    Update First_Bad_version.cpp

--- Implementations/First_Bad_version.cpp
+++ Implementations/First_Bad_version.cpp
@@ -16,3 +16,23 @@ public:
         }   
     }
 };
+class Solution {
+    //better version.
+public:
+    int mn = INT_MAX;
+    int firstBadVersion(int n) {
+        int low = 0,high = n;
+        int mid,res;
+        while(low<=high){
+            mid = high+(low-high)/2;
+            if(isBadVersion(mid)){
+                res = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return res;
+    }
+};
