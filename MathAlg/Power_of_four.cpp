d8b26e83e498be3fff8c538a27d5dc9215fa0674
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-04-30 19:58:00 +0600

    Update Power_of_four.cpp

--- MathAlg/Power_of_four.cpp
+++ MathAlg/Power_of_four.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // attemp1
     bool isPowerOfFour(int n) {
         if(n==1 || n==4){
             return true;
@@ -14,6 +15,25 @@ public:
             else{
                 n /= 4;
             }
+        }
+        return false;
+    }
+};
+class Solution2 {
+public:
+    // atempt 2
+    bool isPowerOfFour(int n) {
+        if(n==1 || n==4){
+            return true;
+        }
+        if(n%4!=0 || n<4){
+            return false;
+        }
+        while(n){
+            if(sqrt(n)==4 || n==4){
+                return true;
+            }
+            n = n>>4;
         }
         return false;
     }
