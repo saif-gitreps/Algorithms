7971bf4ede62c6e9463129cb738ca91588c7a42a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-01 10:57:10 +0600

    Update Eval_reverse_pol_notation.cpp

--- Implementations/Eval_reverse_pol_notation.cpp
+++ Implementations/Eval_reverse_pol_notation.cpp
@@ -54,3 +54,16 @@ public:
         return s.top();
     }
 };
+//this is a java sol that i really liked.
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
+            else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
+            else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
+            else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
+            else stack.push(Integer.valueOf(s));
+        }
+
+        return stack.pop();
+    }
