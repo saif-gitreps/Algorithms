9dc72e517a3cccf218720954cf3060dad136220f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-01 11:50:55 +0600

    Update Rev_words_in_a_string.cpp

--- StringAlg/Rev_words_in_a_string.cpp
+++ StringAlg/Rev_words_in_a_string.cpp
@@ -1,1 +1,30 @@
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        string newWord = "";
+        string word = "";
+        stack<string> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=' '){
+                word += s[i];
+                if(i+1<s.size() && s[i+1]==' '){
+                    st.push(word);
+                }
+                else if(i+1==s.size()){
+                    st.push(word);
+                }
+            }
+            else{
+                word = "";
+            }
+        }
+        while(!st.empty()){
+            newWord += st.top();
+            newWord += " ";
+            st.pop();
+        }
+        int n = newWord.size()-1;
+        newWord.erase(newWord.begin()+n,newWord.end());
+        return newWord;
+    }
+};
