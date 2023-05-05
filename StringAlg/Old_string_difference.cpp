0e3a43465531033da76e444f9f2fe722a5d4f4e8
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-05 23:34:39 +0600

    Create Old_string_difference.cpp

---
+++ StringAlg/Old_string_difference.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string oddString(vector<string>& words) {
+        map<vector<int>,int> m;
+        vector<vector<int>> a;
+        vector<int> b;
+        for(int i =0;i<words.size();i++){
+            string temp = words[i];
+            for(int j=1;j<words[i].size();j++){
+                int num = temp[j]-temp[j-1];
+                b.push_back(num);
+            }
+            a.push_back(b);
+            m[b]++;
+        }
+        
+        for(int i=0;i<a.size();i++){
+            if(m[a[i]]==1){
+                return words[i];
+            }
+        }
+        return "no";
+    }
+};
