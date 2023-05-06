54be4f2fc3ae6212851cc62ab51d201aa819e45c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-06 12:57:10 +0600

    Update Move_Zeroes.cpp

--- Implementations/Move_Zeroes.cpp
+++ Implementations/Move_Zeroes.cpp
@@ -13,3 +13,25 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    /*
+    tthis is the proper solution , it beats 98% in memory but is the last in time complexity.
+    */
+    void moveZeroes(vector<int>& nums) {
+        if(nums.size()==NULL){
+            return;
+        }
+        for(int i =0 ; i<nums.size();i++){
+            if(nums[i]!=0){
+                continue;
+            }
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[j]!=0 && nums[i]==0){
+                    swap(nums[i],nums[j]);
+                }
+            }
+        }
+    }
+};
