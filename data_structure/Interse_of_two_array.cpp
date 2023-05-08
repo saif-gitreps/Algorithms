3d9f446fe231ad8bd0ef6a98a94939a3a4c57735
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-08 16:40:55 +0600

    Update Interse_of_two_array.cpp

--- data_structure/Interse_of_two_array.cpp
+++ data_structure/Interse_of_two_array.cpp
@@ -15,3 +15,21 @@ public:
    return ans;
     }
 };
+class Solution2 {
+public:
+    vector<int> intersection(vector<int>& a, vector<int>& b){
+    vector<int> ans;
+    set<int> s;
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<b.size();j++){
+            if(a[i]==b[j]){
+                s.insert(a[i]);
+            }
+        }
+    }
+    for(auto x: s){
+        ans.push_back(x);
+    }
+    return ans;
+    }
+};
