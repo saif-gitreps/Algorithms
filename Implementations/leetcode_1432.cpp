8970bbde2b36b47e8f6dab8c1911697cadca92c4
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-09 13:34:33 +0600

    Update leetcode_1432.cpp

--- Implementations/leetcode_1432.cpp
+++ Implementations/leetcode_1432.cpp
@@ -19,3 +19,30 @@ public:
         return mx;
     }
 };
+
+class Solution {
+  // damn i cant believe i solved it. 
+public:
+    int maxDiff(int n) {
+        string num = to_string(n);
+        int mx = -1;
+        for(int i=0;i<num.size();i++){
+            string num2 = num;
+            replace(num2.begin(),num2.end(),num2[i],'9');
+            for(int j =0;j<num.size();j++){
+                string num3 = num;
+                if(j>0 && num3[0]!=num[j]){
+                    replace(num3.begin(),num3.end(),num3[j],'0');    
+                }
+                else {
+                    replace(num3.begin(),num3.end(),num3[j],'1');
+                }
+                int n1 = stoi(num2);
+                int n2 = stoi(num3);
+                int res = n1 - n2;
+                mx = max(mx,res);
+            }
+        }
+        return mx;
+    }
+};
