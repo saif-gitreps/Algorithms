2c8c9ec30ff8d8740e453965b7227c3b769c2735
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-11 13:41:52 +0600

    Update Count_and_Say.cpp

--- Implementations/Count_and_Say.cpp
+++ Implementations/Count_and_Say.cpp
@@ -60,3 +60,36 @@ string ans(string num,int i,int n){
     }
 
 };
+
+class Solution {
+public:
+  // finally understood something atleast.
+    string countAndSay(int n){
+   if(n==1){
+      return "1";
+
+   }
+   if(n==2){
+      return "11";
+   }
+   string str = "11";
+   for(int i=3;i<=n;i++){
+      string temp = "";
+      str += "&";
+      int count = 1;
+      for(int j=1;j<str.size();j++){
+         if(str[j]!=str[j-1]){
+            temp+= to_string(count);
+            temp+= str[j-1];
+            count = 1;
+         }
+         else{
+            count++;
+         }
+      }
+      str = temp;
+   }
+   return str;
+}
+
+};
