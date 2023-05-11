040f984eb2a25c455f510e4654f71e43c8b2fa80
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-11 13:14:01 +0600

    Update Count_and_Say.cpp

--- Implementations/Count_and_Say.cpp
+++ Implementations/Count_and_Say.cpp
@@ -26,3 +26,37 @@ public:
          return ans("1",1,n);
     }
 };
+
+class Solution {
+  // yay wroked till 3rd test case.
+public:
+string ans(string num,int i,int n){
+        if(i==n){
+            return num;
+        }
+        else{
+            map<int,int> mp;
+            queue<int> q;
+            for(int k=0;k<num.size();k++){
+                int number = num[k]-'0';
+                mp[number]++;
+                if(q.front()!=number){
+                  q.push(number);
+                }
+            }
+
+            string sender = "";
+            while(q.empty()!=true){
+               sender += mp[q.front()]+48;
+               sender += q.front()+48;
+               q.pop();
+            }
+            return ans(sender,i+1,n);
+        }
+    }
+
+    string countAndSay(int n) {
+         return ans("1",1,n);
+    }
+
+};
