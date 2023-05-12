012777e3516d8cb461fd205be16a76adb0b21212
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-12 21:35:28 +0600

    Update Path_sum.cpp

--- data_structure/Path_sum.cpp
+++ data_structure/Path_sum.cpp
@@ -21,3 +21,30 @@ public:
         return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
     }
 };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution2{
+    // need to study this alg
+public:
+    bool pathSum(TreeNode *root, int targetSum, int sum) {
+        if(root==NULL)
+            return false;
+        sum=sum+root->val;
+        if(root->left==NULL && root->right==NULL && sum==targetSum)
+            return true;
+        return pathSum(root->left, targetSum, sum) || pathSum(root->right, targetSum, sum);
+    }
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum=0;
+        return pathSum(root, targetSum, sum);
+    }
+};
