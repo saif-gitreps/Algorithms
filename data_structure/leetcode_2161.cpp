abe129689a0b28984e6a3a8fbcc4456ecd314acf
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-19 22:44:43 +0600

    Create leetcode_2161.cpp

---
+++ data_structure/leetcode_2161.cpp
@@ -0,0 +1,33 @@
+class Solution {
+  // first attempt, i worked hard on this one ,didnt want to discard them.
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> a;
+        vector<int> same;
+        priority_queue<int> pq;
+        set<int> s;
+        for(int i=0;i<nums.size();i++){ 
+            if(nums[i]==pivot){
+                same.push_back(nums[i]);
+            }
+            else if(nums[i]>pivot){
+                s.insert(nums[i]);
+            }
+            else{
+                pq.push(nums[i]);
+            }
+        }
+        while(pq.empty()!=true){
+            a.push_back(pq.top());
+            pq.pop();
+        }
+        for(auto x:same){
+            a.push_back(x);
+        }
+        for(auto x: s){
+            a.push_back(x);
+        }
+        return a;
+        
+    }
+};
