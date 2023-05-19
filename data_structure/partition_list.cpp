c71715980dfb7333d8749bc8c6e08bb44ec2d066
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-19 21:57:25 +0600

    Update partition_list.cpp

--- data_structure/partition_list.cpp
+++ data_structure/partition_list.cpp
@@ -39,3 +39,49 @@ public:
         return reference->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+  // SUIIIIIIIIIIIIIII, it worked,my original idea stands althought i ate alot of memory .
+  // my mistake was when i was pushing the nodes in queue and set , i was not pushing nodes instead i was pushing addreses , thats why
+  // i was getting the memory access error , C++ sucks .
+public:
+    ListNode* partition(ListNode* head, int x) {
+        set<ListNode*> s;
+        queue<ListNode*> orig;
+        ListNode *reference,*temp,*ptr = head;
+        while(ptr!=NULL){
+            if(ptr->val<x){
+                ListNode *value = new ListNode(ptr->val);
+                s.insert(value);
+            }
+            else{
+                ListNode *value = new ListNode(ptr->val);
+                orig.push(value);
+            }
+            ptr=ptr->next;
+        }
+        ListNode *newHead = new ListNode(0);
+        reference = newHead;
+        for(auto x: s){
+            newHead->next = x;
+            newHead = newHead->next;
+        }
+        while(orig.empty()!=true){
+            newHead->next = orig.front();
+            newHead = newHead->next;
+            orig.pop();
+        }
+    ListNode* result = reference->next;
+    return result;
+    }
+};
