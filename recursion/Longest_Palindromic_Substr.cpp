ce71388b16272b6ad955529754fa4c2b4685eaf1
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-19 21:10:54 +0600

    Update Longest_Palindromic_Substr.cpp

--- recursion/Longest_Palindromic_Substr.cpp
+++ recursion/Longest_Palindromic_Substr.cpp
@@ -20,3 +20,34 @@ public:
         }
     }
 };
+
+class Solution {
+    // brute force approach , works till 40 cases, then shows SLE (space limit exceeding).
+public:
+    bool check(string s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int starting_index = 0;
+        int max_length = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(check(s,i,j)==true){
+                    if(j-i+1>max_length){
+                        max_length = j-i+1;
+                        starting_index = i;
+                    }
+                }
+            }
+        }
+        return s.substr(starting_index,max_length);
+    }
+};
