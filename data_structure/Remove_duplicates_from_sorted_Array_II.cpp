d5257b3682c942ce6a6b0a2d58e1c183a8a02dc3
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-20 17:23:03 +0600

    Create Remove_duplicates_from_sorted_Array_II.cpp

---
+++ data_structure/Remove_duplicates_from_sorted_Array_II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  // first attempt, shows TLE for some reason. 
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int limit = n-1,dup = 0;
+        for(int i=0;i+2<n;i++){
+            if(nums[i]==nums[i+1] && nums[i+1]==nums[i+2]){
+                dup++;
+                int k = i+2;
+                while(k+1<n){
+                    swap(nums[k],nums[k+1]);
+                    k++;
+                }
+                i--;
+            }
+
+        }
+        return n-dup;
+    }
+};
