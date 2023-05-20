7903302a0beed620ea6c278e6745552f31feb8aa
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-20 19:40:36 +0600

    Update Remove_duplicates_from_sorted_Array_II.cpp

--- data_structure/Remove_duplicates_from_sorted_Array_II.cpp
+++ data_structure/Remove_duplicates_from_sorted_Array_II.cpp
@@ -94,3 +94,16 @@ public:
         return n-dup;
     }
 };
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        for(auto n : nums){
+            if(i==0||i==1||nums[i-2]!=n){
+                nums[i]=n;
+                i++;
+            }
+        }
+        return i;
+    }
+};
