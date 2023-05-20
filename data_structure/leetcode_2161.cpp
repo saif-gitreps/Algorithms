2128bcbac4f4ab3e08a068d3288f89c40d510886
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-20 11:03:01 +0600

    Update leetcode_2161.cpp

--- data_structure/leetcode_2161.cpp
+++ data_structure/leetcode_2161.cpp
@@ -31,3 +31,34 @@ public:
         
     }
 };
+class Solution {
+  // worked till 14 cases, then the test case became weird, i dont know why
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> a;
+        vector<int> same;
+        priority_queue<int> pq;
+        for(int i=0;i<nums.size();i++){ 
+            if(nums[i]==pivot){
+                same.push_back(nums[i]);
+            }
+            else if(nums[i]<pivot){
+                pq.push(nums[i]);
+            }
+        }
+        while(pq.empty()!=true){
+            a.push_back(pq.top());
+            pq.pop();
+        }
+        for(auto x:same){
+            a.push_back(x);
+        }
+        for(auto x:nums){
+            if(x>pivot){
+                a.push_back(x);
+            }
+        }
+        return a;
+        
+    }
+};
