c115c174228e96a49ef3eb112d5687db920ec749
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-22 20:19:49 +0600

    Update Number of common factors.cpp

--- MathAlg/Number of common factors.cpp
+++ MathAlg/Number of common factors.cpp
@@ -12,3 +12,30 @@ public:
         return count;
     }
 };
+class Solution {
+    // note : this doesnt work,but i like the concept and the idea .
+public:
+    vector<int> factors(int n,int m){
+        vector<int> a;
+        for(int i=2;i*i<=n && i*i<=m;i++){
+            while(n%i==0 && m%2==0){
+                a.push_back(i);
+                n = n/i;
+                m = m/i;
+            }
+        }
+        if(n==m && n>1){
+            a.push_back(n);
+        }
+        return a;
+    }
+    int commonFactors(int a, int b) {
+        int i=0,j=0,count = 1;
+        if(max(a,b)%min(a,b)==0){
+            count++;
+        }
+        vector<int> v = factors(a,b);
+        count += v.size();
+        return count;
+    }
+};
