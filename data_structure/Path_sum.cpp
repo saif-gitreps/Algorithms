03856b6b24dad76133f5b77f47cfc8ae99eeb175
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-22 19:12:40 +0600

    Update Path_sum.cpp

--- data_structure/Path_sum.cpp
+++ data_structure/Path_sum.cpp
@@ -48,3 +48,32 @@ public:
         return pathSum(root, targetSum, sum);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    // this version , i understood. we take true or false and store it in a two different variables. and then check if either of variables 
+    // bring us true , else we return false.
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL){
+      return false;
+   }
+   if(root->left==NULL && root->right==NULL && root->val==targetSum){
+      return true;
+   }
+   bool left = hasPathSum(root->left,targetSum-root->val);
+   bool right = hasPathSum(root->right,targetSum-root->val);
+
+   return ((left||right)?true:false);
+    }
+};
