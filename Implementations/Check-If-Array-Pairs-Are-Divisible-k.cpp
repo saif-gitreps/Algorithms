7250e64e22f4a09465d25e353a971052cc0a37fb
Saif <droconos99@gmail.com>
2023-05-24 23:44:52 +0600

    updated LC 1497

--- Implementations/Check-If-Array-Pairs-Are-Divisible-k.cpp
+++ Implementations/Check-If-Array-Pairs-Are-Divisible-k.cpp
@@ -30,3 +30,38 @@ public:
         }
     }
 };
+
+// [note:] interesting property  , the mod sum of two numbers 
+// will be either the K or 0 (if two number are divisble)
+
+class Solution {
+    // this solution works, need to see it again.
+public:
+    bool canArrange(vector<int>& a, int k) {
+           unordered_map<int,int> mp;
+   // map still store each element's mod with the K
+   // so what he explains is those numbers with mod 1
+   // should be equal to the numbers that give mod 4
+   // similarly those number whic have mod 2 should be 
+   // equal number to mod 3 (for k=5)
+   // if they are not equal , we have to return false. 
+   for(int i=0;i<a.size();i++){
+      int num = (a[i]%k+k)%k;
+      // this is done to deal to with negative numbers.
+      mp[num]++;
+   }
+   if(mp[0]%k==1){
+      return false;
+   }
+   for(int i=1;i<k;i++){
+      // this basically means count of 1%5(1)
+      // == count of 4%5(4) i.e 1+4 == 5.
+      // if that isnt equal then we print false.
+      if(mp[i]!=mp[k-i]){
+         return false;
+      }
+   }
+   return true;
+
+    }
+};
