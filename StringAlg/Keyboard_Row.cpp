eaf0ea10d28fdb7145cb3bc958a7461e891a1009
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-05-28 23:09:02 +0600

    Update Keyboard_Row.cpp

--- StringAlg/Keyboard_Row.cpp
+++ StringAlg/Keyboard_Row.cpp
@@ -29,3 +29,55 @@ public:
 
     }
 };
+
+class Solution {
+  //attempt 2;
+public:
+    vector<string> findWords(vector<string>& words) {
+        string u = "eiopqrtuwy";
+        string m = "adfghjkls";
+        string d = "bcmnvxz";
+        vector<string> ans;
+        for(int i=0;i<words.size();i++){
+            string temp = words[i];
+            set<char> tem;
+            for(auto x: temp){
+                tem.insert(x);
+            }
+            int j =0;
+            for(auto s: tem){
+                if(s!=u[j]){
+                    ++j;
+                    break;
+                }
+            }
+            if(j==tem.size()){
+                ans.push_back(temp);
+                continue;
+            }
+            j =0;
+            for(auto s: tem){
+                if(s!=m[j]){
+                    ++j;
+                    break;
+                }
+            }
+            if(j==tem.size()){
+                ans.push_back(temp);
+                continue;
+            }
+            j =0;
+            for(auto s: tem){
+                if(s!=d[j]){
+                    ++j;
+                    break;
+                }
+            }
+            if(j==tem.size()){
+                ans.push_back(temp);
+                continue;
+            }
+        }
+        return ans;
+    }
+};
