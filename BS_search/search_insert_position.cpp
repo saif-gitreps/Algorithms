7fc7ef462e51f0aa8132ba75a8ded66e959f4ac7
Saif <droconos99@gmail.com>
2023-06-01 22:51:14 +0600

    updated LC35

--- BS_search/search_insert_position.cpp
+++ BS_search/search_insert_position.cpp
@@ -69,3 +69,32 @@ public:
            
     }
 };
+
+class Solution {
+    // made the condition even more specific, works now til 42 test cases.
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int mid,left =0,right = nums.size()-1;
+        while(left<right){
+            mid = left + (right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        if(target>nums[left]){
+            return left+1;
+        }
+        else if(target<nums[0]){
+            return 0;
+        }
+        else{
+            return left+2;
+        }  
+    }
+};
