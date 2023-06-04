2fbb4f8f2ae81dbf5ef1517c508b206a832d2b44
Saif <droconos99@gmail.com>
2023-06-04 21:06:19 +0600

    added combination sum II

--- recursion/CombinationSumII.cpp
+++ recursion/CombinationSumII.cpp
@@ -46,3 +46,50 @@ public:
         return finally;
     }
 };
+
+class Solution {
+    // attemp 2 : worked till 23 test cases then my memory limited exceeded.
+public:
+    void rev(int i,vector<int> a,set<vector<int>>& final_ds,vector<int>& ds,int sum,int target){
+        if(i==a.size()){
+            if(sum==target){
+                final_ds.insert(ds);
+                return;
+            }
+            return;
+        }
+        sum += a[i];
+        ds.push_back(a[i]);
+        rev(i+1,a,final_ds,ds,sum,target);
+        sum -= a[i];
+        ds.pop_back();
+        rev(i+1,a,final_ds,ds,sum,target);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        set<vector<int>> final_ds;
+        vector<vector<int>> finally;
+        vector<int> ds;
+        map<int,int> mp;
+        sort(candidates.begin(),candidates.end());
+        rev(0,candidates,final_ds,ds,0,target);
+        for(auto s : candidates){
+            mp[s]++;
+        }
+        for(auto s: final_ds){
+            int i =0;
+            for(;i<s.size();i++){
+               int temp = mp[s[i]]; 
+                if(temp>=1){
+                    temp--;
+                }
+                else{
+                  break;
+                }
+            }
+            if(i==s.size()){
+                    finally.push_back(s);
+                }
+        }
+        return finally;
+    }
+};
