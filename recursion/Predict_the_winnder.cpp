c5b9bf770828a276c7141abd0b389917b2bc9450
Saif <droconos99@gmail.com>
2023-06-08 23:52:04 +0600

    uodated

--- recursion/Predict_the_winnder.cpp
+++ recursion/Predict_the_winnder.cpp
@@ -63,3 +63,39 @@ public:
         return check(0,nums.size()-1,nums,0,0);   
     }
 };
+
+class Solution {
+    // worked till 55 cases.
+public:
+    bool check(int left,int right,vector<int> a,int p1,int p2){
+        if(a.size()==1){
+                return true;
+            }
+        if(right-left==2 && a.size()%2!=0){
+            if(a[left]+a[right]>a[left+1]){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        if(right-left==1){
+            if(p1+a[right]>=p2+a[left]){
+                return true;
+            }
+            else if(p1+a[left]>=p2+a[right]){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        bool checking1 = check(left+1,right-1,a,p1+a[left],p2+a[right]);
+        bool checking2 = check(left+1,right-1,a,p1+a[right],p2+a[left]);
+        return (checking1||checking2)? true:false;
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+    
+        return check(0,nums.size()-1,nums,0,0);   
+    }
+};
