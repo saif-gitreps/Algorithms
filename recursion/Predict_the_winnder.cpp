f16864a0a6b30ef75b4fb634d85537db2dca7061
Saif <droconos99@gmail.com>
2023-06-08 20:53:11 +0600

    updated combination sum II

--- recursion/Predict_the_winnder.cpp
+++ recursion/Predict_the_winnder.cpp
@@ -29,3 +29,37 @@ public:
         return check(0,nums.size()-1,nums,0,0);   
     }
 };
+
+class Solution {
+    // worked till 52 test cases.
+public:
+    bool check(int left,int right,vector<int> a,int p1,int p2){
+        if(left==right){
+            // if(p1+a[left]>=p2){
+            //     return true;
+            // }
+            if(a.size()==1){
+                return true;
+            }
+            return false;
+        }
+        if(right-left==1){
+            if(p1+a[right]>=p2+a[left]){
+                return true;
+            }
+            else if(p1+a[left]>=p2+a[right]){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        bool checking1 = check(left+1,right-1,a,p1+a[left],p2+a[right]);
+        bool checking2 = check(left+1,right-1,a,p1+a[right],p2+a[left]);
+        return (checking1||checking2)? true:false;
+    }
+    bool PredictTheWinner(vector<int>& nums) {
+    
+        return check(0,nums.size()-1,nums,0,0);   
+    }
+};
