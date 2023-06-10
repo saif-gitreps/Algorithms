819c01d166c8038c4b0cbe2ceda03ba2ee96ab5c
Saif <droconos99@gmail.com>
2023-06-10 16:21:19 +0600

    sum in a matrix

---
+++ Implementations/Sum_in_matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        int mx = -1; int mx_index = 0;
+        int sum = 0; int indx =0;
+        for(int i=0;i<nums[0].size();){
+            if(indx>=nums.size()){
+                indx = 0;
+            }
+            mx_index = -1;
+            mx = -1;
+            vector<int> temp = nums[indx];
+            vector<int> deleting_arr;
+            for(int j =0;j<temp.size();j++){
+                if(temp[j]>mx){
+                    mx = temp[j];
+                    mx_index = j; 
+                }
+            }
+            deleting_arr.push_back(mx_index);
+            int final_mx = -1;
+            if(indx==nums.size()-1){
+                for(auto x: deleting_arr){
+                    final_mx = max(final_mx,temp[x]);
+                    temp[x] = -69;
+                }
+                i++;
+                sum += final_mx;
+            }
+            indx++;
+        }
+        return sum;
+    }
+};
