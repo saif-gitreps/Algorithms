f0dcafbe82f100d1a79699a13f80c397ddf25de2
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-06-11 22:00:24 +0600

    Update Continas_duplicate.cpp

--- Implementations/Continas_duplicate.cpp
+++ Implementations/Continas_duplicate.cpp
@@ -10,3 +10,19 @@ public:
         return false;
     }
 };
+class Solution {
+public:
+    // apparently O(n) is more slower than the frist one. 
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(mp[nums[i]]>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
