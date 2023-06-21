e0b0e11e66dd85aa551b7a80a3dd753e9fd43d2b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-06-21 23:38:27 +0600

    Update Distant_Barcodes.cpp

--- Implementations/Distant_Barcodes.cpp
+++ Implementations/Distant_Barcodes.cpp
@@ -59,3 +59,36 @@ public:
     }
 
 };
+
+
+class Solution {
+public:
+    vector<int> rearrangeBarcodes(vector<int>& b) {
+        if(b.size()<=1){
+            return b;
+        }
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<b.size();i++){
+            mp[b[i]]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(auto x: mp){
+            pq.push({x.second,x.first});
+        }
+        while(pq.top().first>0){
+            pair<int,int> pair1 = pq.top();pq.pop();
+            ans.push_back(pair1.second);
+            pair1.first--;
+            pair<int,int> pair2 = pq.top();pq.pop();
+            if(pair2.first>0){
+                ans.push_back(pair2.second);
+                pair2.first--;
+            }
+            pq.push(pair1);
+            pq.push(pair2);
+        }
+        return ans;
+    }
+
+};
