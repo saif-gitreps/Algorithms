edac7a629381c8644c4d089ef30435137370385a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-04 12:48:07 +0600

    Update Permutataions.cpp

--- recursion/Permutataions.cpp
+++ recursion/Permutataions.cpp
@@ -23,3 +23,24 @@ public:
         return ans;
     }
 };
+
+class Solution {
+// best solution in terms of space complexity and time complexity.
+public:
+    void kev(int ind,vector<int> &a,vector<vector<int>> &ans){
+        if(ind>=a.size()){
+            ans.push_back(a);
+            return;
+        }
+        for(int i=ind;i<a.size();i++){
+            swap(a[ind],a[i]);
+            kev(ind+1,a,ans);
+            swap(a[ind],a[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        kev(0,nums,ans);
+        return ans;
+    }
+};
