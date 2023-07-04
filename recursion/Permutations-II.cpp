e2e1dd29b97a77b3a59cec5fc3294c7e56aa1a5b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-04 12:52:19 +0600

    Update Permutations-II.cpp

--- recursion/Permutations-II.cpp
+++ recursion/Permutations-II.cpp
@@ -21,3 +21,30 @@ public:
         return ans;
     }
 };
+class Solution {
+// best in terms of space complexity but works for 14/33 test cases only.
+public:
+    void kev(int i,vector<int> &a,vector<vector<int>> &ans){
+        if(i>=a.size()){
+            ans.push_back(a);
+        }
+        for(int j = i;j<a.size();j++){
+            if(j!=i && a[j]==a[i]){
+                continue;
+            }
+            swap(a[j],a[i]);
+            kev(i+1,a,ans);
+            swap(a[j],a[i]);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        // set<vector<int>> s;
+        vector<vector<int>> ans;
+        kev(0,nums,ans);
+        // for(auto x:s){
+        //     ans.push_back(x);
+        // }
+        return ans;
+    }
+};
+
