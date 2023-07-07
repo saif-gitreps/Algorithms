22376c9cf17c2146eb437f68b31953a97daea3d9
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-07 21:14:33 +0600

    Update Swapping-Nodes-In-a-LinkedList.cpp

--- data_structure/Swapping-Nodes-In-a-LinkedList.cpp
+++ data_structure/Swapping-Nodes-In-a-LinkedList.cpp
@@ -31,3 +31,31 @@ public:
         return head;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *leftPtr=head,*left_marked,*rightPtr=head;
+        //int i=0;
+        for(int i=1;i<k;i++){
+            leftPtr = leftPtr->next;
+        }
+        left_marked = leftPtr;
+        while(leftPtr->next!=NULL){
+            rightPtr= rightPtr->next;
+            leftPtr = leftPtr->next;
+        }
+        swap(left_marked->val,rightPtr->val);
+        return head;
+    }
+};
