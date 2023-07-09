a704162f5abb56d0d3a325c5ea9b5d21d4b4f285
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-09 20:31:58 +0600

    Update Longest-Arithmetic-subsequence.cpp

--- recursion/Longest-Arithmetic-subsequence.cpp
+++ recursion/Longest-Arithmetic-subsequence.cpp
@@ -42,3 +42,65 @@ int main(){
    }
    cout<<mx<<" ";
 }
+
+// atempt2 . still bugs around the code.
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define newline() "\n"
+#define enter_arr(x,n) for(int i=0;i<n;i++)cin>>x[i]
+#define out_arr(x,n) for(int i=0;i<n;i++)cout<<x[i]<<" "
+#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
+#define GGG 9223372036854775807
+#define pb push_back
+#define FAST(SUIII); ios_base::sync_with_stdio(false);cin.tie(NULL);
+const int MAXN = 100010;
+
+void kev(int i,vector<int> a,stack<int> &s,unordered_map<int,int> &mp,unordered_map<int,int> &avail){
+   if(i>=a.size()){
+      return;
+   }
+   if(s.size()==2){
+      int num1 = s.top();s.pop();
+      int num2 = s.top();s.pop();
+      int mx  = max(num1,num2);
+      int mn = min(num1,num2);
+      if(avail[mx]==mn || num1==num2){
+         s.push(num2);
+         s.push(num1);
+         return;
+      }
+      else{
+         avail[mx]=mn; 
+      }
+      int diff = abs(num1-num2);
+      mp[diff]++;
+      s.push(num2);
+      s.push(num1);
+      debug(num1);
+      debug(num2);
+      debug(diff);
+      debug(mp[diff]);
+      debug("-======");
+      return;
+   }
+   for(int j=i;j<a.size();j++){
+      s.push(a[j]);
+      kev(i+1,a,s,mp,avail);
+      s.pop();
+   }
+}
+
+int main(){
+   vector<int> a = {20,1,15,3,10,5,8};
+   unordered_map<int,int> mp;
+   unordered_map<int,int> avail;
+   stack<int> s;
+   kev(0,a,s,mp,avail);
+   int mx = 0;
+   for(auto x:mp){
+      mx = max(x.second,mx);
+   }
+   cout<<mx<<" ";
+}
