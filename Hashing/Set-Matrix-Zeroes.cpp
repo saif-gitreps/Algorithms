e2d8c6c5d455a4ecd5eb7e57cd6e05c47b1df458
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-17 19:28:06 +0600

    Create Set-Matrix-Zeroes.cpp

---
+++ Hashing/Set-Matrix-Zeroes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+// first attempt , passed till 168/170 cases. damn 
+public:
+    void setZeroes(vector<vector<int>>& a) {
+        int row = a[0].size();
+        int col = a.size();
+        int count = 0;
+        vector<pair<int,int>> v;
+        for(int i=0;i<col;i++){
+            for(int j=0;j<row;j++){
+                if(a[i][j]==0){
+                    v.push_back({i,j});
+                    count++;
+                }
+            }
+        }
+        for(int i=0;i<col;i++){
+            if(count==((row+1)*(col+1))){
+                break;
+            }
+            for(int j=0;j<row;j++){
+                if(a[i][j]==0){
+                    continue;
+                }
+                for(auto s: v){
+                    if(s.first==i || s.second==j){
+                        a[i][j] = 0;
+                        count++;
+                    }
+                }
+            }
+        }
+        
+    }
+};
