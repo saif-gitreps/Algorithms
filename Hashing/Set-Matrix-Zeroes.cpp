d6aa8f907bfdfcad0011c9ba2647c8c26731bd47
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-17 19:57:47 +0600

    Update Set-Matrix-Zeroes.cpp

--- Hashing/Set-Matrix-Zeroes.cpp
+++ Hashing/Set-Matrix-Zeroes.cpp
@@ -2,6 +2,7 @@ class Solution {
 // first attempt , passed till 168/170 cases. damn 
 // second attempt, commented out that condition , and it passed but it was the slowest algorithm ever.
 // third, added a break in the hash vector iterator.
+// fourth, now i uncommented the condition , that made my code beat 80% in memeory but worse in time .
 public:
     void setZeroes(vector<vector<int>>& a) {
         int row = a[0].size();
@@ -17,9 +18,9 @@ public:
             }
         }
         for(int i=0;i<col;i++){
-            // if(count==((row+1)*(col+1))){
-            //     break;
-            // }
+            if(count==((row+1)*(col+1))){
+                break;
+            }
             for(int j=0;j<row;j++){
                 if(a[i][j]==0){
                     continue;
