9ef21625aecb427d4a3e187e45ee21857df09c03
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-19 20:20:01 +0600

    Update N-QUEENS.cpp

--- recursion/N-QUEENS.cpp
+++ recursion/N-QUEENS.cpp
@@ -55,3 +55,46 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+// best in terms of time complexity.
+public:
+    void kev(int col,vector<string> &board,vector<vector<string>> &ans,vector<int> &left_row,vector<int> &upper_diagonal,vector<int> &lower_diagonal,int n){
+        // concept to rememeber about diagonal,
+        // for checking lower diagonal and upper diagonal , keep a hash array of size
+        // 2*n - 1. 
+        // hash row + col for checking lower diagonal.
+        // hash n-1 + (row-col);
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(left_row[row]==0 && 
+            lower_diagonal[row + col]==0 &&
+            upper_diagonal[n-1 + (col - row)]==0){
+                board[row][col] = 'Q';
+                left_row[row] = 1;
+                lower_diagonal[row+col] = 1;
+                upper_diagonal[n-1 + (col - row)] = 1;
+                kev(col+1,board,ans,left_row,upper_diagonal,lower_diagonal,n);
+                board[row][col] = '.';
+                left_row[row] = 0;
+                lower_diagonal[row+col] = 0;
+                upper_diagonal[n-1 + (col - row)] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<board.size();i++){
+            board[i] = s; 
+        }
+        vector<int> left_row(n,0),upper_diagonal(2*n-1,0),lower_diagonal(2*n-1,0);
+        kev(0,board,ans,left_row,upper_diagonal,lower_diagonal,n);
+        return ans;
+    }
+};
