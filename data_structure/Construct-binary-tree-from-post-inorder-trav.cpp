844c231c3731eb5cad20f037b972614d3cb6f18e
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-24 19:17:52 +0600

    Update Construct-binary-tree-from-post-inorder-trav.cpp

--- data_structure/Construct-binary-tree-from-post-inorder-trav.cpp
+++ data_structure/Construct-binary-tree-from-post-inorder-trav.cpp
@@ -39,3 +39,42 @@ public:
         return root;
     }
 };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+// so the problem weas , i needed a nums left varibale , from start till nums left
+// idk why the root_index varibale logic didnt work.
+public:
+    TreeNode* build(vector<int> &inorder,int in_left,int in_right,vector<int> &post,int post_left,int post_right,map<int,int> &mp){
+        if(in_left>in_right || post_left>post_right){
+            return NULL;
+        }
+        TreeNode *root = new TreeNode(post[post_right]);
+        int root_index = mp[root->val];
+        int nums_left = root_index-in_left;
+        root->left = build(inorder,in_left,root_index-1,post,post_left,post_left+nums_left-1,mp);
+        root->right = build(inorder,root_index+1,in_right,post,post_left+nums_left,post_right-1,mp);
+        return root;
+    }
+    
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& post) {
+        if(inorder.size()!=post.size()){
+            return NULL;
+        }
+        map<int,int> mp;
+        for(int i=0;i<inorder.size();i++){
+            mp[inorder[i]] = i;
+        }
+        return build(inorder,0,inorder.size()-1,post,0,post.size()-1,mp);
+    }
+};
+
