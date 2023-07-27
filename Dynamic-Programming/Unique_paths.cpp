49de3a7a8b8494c6a02cd88ebf5b3783e3abea72
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-27 20:10:35 +0600

    Update Unique_paths.cpp

--- Dynamic-Programming/Unique_paths.cpp
+++ Dynamic-Programming/Unique_paths.cpp
@@ -42,3 +42,29 @@ int main(){
    kev(0,0,m,n,ans);
    cout<<ans;
 }
+
+class Solution {
+// holyy sheet this worked, but it shows TLE, worked till 50 cases.
+public:
+    void kev(int m,int n,int row,int col,int &ans){
+        if(m==row && n==col){
+            ans += 1;
+            return;
+        }
+        if(m<row){
+            m += 1;
+            kev(m,n,row,col,ans);
+            m -= 1;
+        }
+        if(n<col){
+            n += 1;
+            kev(m,n,row,col,ans);
+            n -= 1;
+        }
+    } 
+    int uniquePaths(int m, int n) {
+        int ans =0;
+        kev(1,1,m,n,ans);
+        return ans;
+    }
+};
