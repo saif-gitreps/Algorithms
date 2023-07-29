b17aae1887d335dc1a9ed5b8b808f1e994370705
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-29 10:25:25 +0600

    Update Add_two_numbers.cpp

--- Data-Structures/Add_two_numbers.cpp
+++ Data-Structures/Add_two_numbers.cpp
@@ -88,3 +88,67 @@ public:
         return dummy->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+// my attempt. i mean i am happy it worked, its O(p+q+s+t) in time and space O(s1+s2+ans) so yea not a memory efficient code
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        queue<ListNode*> s1,s2;
+        stack<ListNode*> ans;
+        ListNode *ptr1 = l1,*ptr2 = l2;
+        int carry = 0;
+        while(ptr1!=NULL){
+            s1.push(ptr1);
+            ptr1 = ptr1->next;
+        }
+        while(ptr2!=NULL){
+            s2.push(ptr2);
+            ptr2 = ptr2->next;
+        }
+        while(!s1.empty() || !s2.empty() || carry){
+            int num1,num2,res;
+            num1 = (!s1.empty())?s1.front()->val:0;
+            num2 = (!s2.empty())?s2.front()->val:0;
+            
+            if(s1.empty()!=true){s1.pop();}
+            if(s2.empty()!=true){s2.pop();}
+            
+            
+            res = num1+num2+carry;
+            
+            ListNode *temp = new ListNode();
+            
+            if(res<=9){
+                temp->val = res;
+                ans.push(temp);
+                carry = 0;
+            }
+            else if(res>9){
+                temp->val = res%10;
+                ans.push(temp);
+                carry = res/10;
+            }
+        }
+        ListNode *head,*ptr;
+        head = ans.top(); ans.pop();
+        ptr = head;
+        ptr->next = NULL;
+        while(!ans.empty()){
+            ListNode *tmp = new ListNode(); 
+            tmp = ans.top(); ans.pop();
+            tmp->next = ptr;
+            ptr = tmp;
+        }
+        return ptr;
+    }
+};
