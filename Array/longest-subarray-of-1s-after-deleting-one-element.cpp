cf2e541ad676b73f6096e8f4811a6d227dd176d8
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-07-31 23:24:17 +0600

    Update longest-subarray-of-1s-after-deleting-one-element.cpp

--- Array/longest-subarray-of-1s-after-deleting-one-element.cpp
+++ Array/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -26,3 +26,32 @@ public:
         return max_seq==a.size()?max_seq-1:max_seq;
     }
 };
+
+class Solution {
+// worked till 75 test cases.
+public:
+    int longestSubarray(vector<int>& a) {
+        int marked = -1;
+        int seq = 0;
+        int max_seq = -1;
+        for(int i=0;i<a.size();i++){
+            if(a[i]==0){
+                if(marked==-1){
+                    marked = i;
+                }
+                else if(marked!=-1){
+                    seq = abs((marked+1)-i);
+                    marked = i;
+                }
+            }
+            if(a[i]==1){
+                seq++;
+                max_seq=max(max_seq,seq);
+            }
+        }
+        if(seq==0 || seq==1){
+            return seq;
+        }
+        return max_seq==a.size()?max_seq-1:max_seq;
+    }
+};
