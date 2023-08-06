ce87d4c9bc02089ddf838dc7de0d67db0f3324b6
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-06 20:26:29 +0600

    Update Balanced-Round.cpp

--- Adhoc/Balanced-Round.cpp
+++ Adhoc/Balanced-Round.cpp
@@ -27,4 +27,33 @@ int main(){
     int t; cin>>t; while(t--)
     solve(); 
 }
+==================================
+   #include <bits/stdc++.h>
+using namespace std;
+#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
+#define GGG 9223372036854775807
+const int MAXN = 100010;
+  
+void solve(){
+   int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for(int i = 0; i < n; ++i) cin >> a[i];    
+    sort(a.begin(),a.end());
+    int cnt = 1, ans = 1;
+    for(int i = 1; i < n; ++i) {
+        if(a[i] - a[i - 1] > k) {
+            cnt = 1;
+        } else {
+            ++cnt;
+        }
+        ans = max(ans, cnt);
+    }
+    cout << n - ans<< '\n';
+}
+
+int main(){
+   
+    int t; cin>>t; while(t--)
+    solve(); 
+}
  
