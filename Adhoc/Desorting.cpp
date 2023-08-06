15ec8c5c647646e5bf9da83681f3e32948b9e4ad
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-06 20:28:10 +0600

    Update Desorting.cpp

--- Adhoc/Desorting.cpp
+++ Adhoc/Desorting.cpp
@@ -35,4 +35,35 @@ int main(){
 }
 
 =====================================================
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
+#define GGG 9223372036854775807
+const int MAXN = 100010;
   
+void solve(){
+   int n; cin >> n;
+        vector<int> nums(n);
+        int diff = 1e9;
+        bool sorted = true;
+        for (int i = 0; i < n; i++) {
+            cin >> nums[i];
+            if (i > 0) {
+                diff = min(nums[i] - nums[i - 1], diff);
+                sorted &= nums[i] >= nums[i - 1];
+            }
+        }
+        
+        if (!sorted) {
+            cout << 0 << "\n";
+            continue;
+        }
+    
+        cout << diff/2 + 1 << "\n";
+}
+
+int main(){  
+    int t; cin>>t; while(t--)
+    solve(); 
+}
+   
