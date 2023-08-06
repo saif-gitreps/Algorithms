add3fd779e96f3ae122179c21753b8bb7624b3d3
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-06 21:29:55 +0600

    Update Maximum-sum.cpp

--- Adhoc/Maximum-sum.cpp
+++ Adhoc/Maximum-sum.cpp
@@ -37,4 +37,36 @@ int main(){
     int t; cin>>t; while(t--)
     solve(); 
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
+#define GGG 9223372036854775807
+const int MAXN = 100010;
+  
+void solve(){
+   int n, k; cin >> n >> k;
+ 
+        vector<int> a(n);
+        for (auto &i : a) cin >> i;
+ 
+        sort(a.begin(), a.end());
+ 
+        vector<long long> psum(n + 1);
+        for (int i = 0; i < n; ++i) {
+            psum[i + 1] = psum[i] + a[i];
+        }
+ 
+        long long answer = 0;
+        for (int i = 0; i <= k; ++i) {
+            answer = max(answer, psum[n - i] - psum[2 * (k - i)]);
+        }
+ 
+        cout << answer << "\n";
+}
+
+int main(){  
+    int t; cin>>t; while(t--)
+    solve(); 
+}
  
