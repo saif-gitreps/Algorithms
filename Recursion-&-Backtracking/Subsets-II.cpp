999aa839158a879e260a5ce5cd2225fceb3cea43
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-08 19:01:38 +0600

    Update Subsets-II.cpp

--- Recursion-&-Backtracking/Subsets-II.cpp
+++ Recursion-&-Backtracking/Subsets-II.cpp
@@ -20,3 +20,29 @@ public:
         return arr;
     }
 };
+
+class Solution {
+//works 100%
+public:
+    void rev(int i,vector<int> a,vector<int> &ds,vector<vector<int>> &ans){
+        if(i>=a.size()){
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(a[i]);
+        rev(i+1,a,ds,ans);
+        ds.pop_back();
+        while(i+1<a.size() && a[i]==a[i+1]){
+            // ex 144445 , takes only one , skips all.
+            i++;
+        }
+        rev(i+1,a,ds,ans);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        rev(0,nums,ds,ans);
+        return ans;
+    }
+};
