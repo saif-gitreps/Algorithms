c8b44e72dceca7657b4262e37652e158aa1295ec
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-09 23:52:15 +0600

    Update maximum-number-of-achievable-transfer-requests.cpp

--- Recursion-&-Backtracking/maximum-number-of-achievable-transfer-requests.cpp
+++ Recursion-&-Backtracking/maximum-number-of-achievable-transfer-requests.cpp
@@ -28,3 +28,38 @@ public:
         return ans;
     }
 };
+
+
+=============================================================================
+#include <bits/stdc++.h>
+    //naive attempt , the concept was there i just wished i knew i had to use backtracking bruteforces
+using namespace std;
+const int N = 1e5 + 10;
+vector<vector<int>> g(N);
+vector<bool> visit(N);
+vector<int> level(N);
+
+int maximumRequests(int n, vector<vector<int>>& a) {
+        vector<int> buildings(n,0);
+        for(int i=0;i<a.size();i++){
+            buildings[a[i][0]]--;
+            buildings[a[i][1]]++;
+        }
+        int count =0;
+        for(int i=0;i<buildings.size();i++){
+            debug(buildings[i]);
+            if(buildings[i]==0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+int main() {
+    vector<vector<int>> v = {{0,1},{1,0},{0,1},{1,2},{2,0},{3,4}};
+
+    cout<<maximumRequests(5,v);
+
+    return 0;
+}
+ 
