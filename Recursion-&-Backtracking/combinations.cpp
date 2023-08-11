eef734a2c8bdf8832a4e2be7e5b43117c189180a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-11 22:52:24 +0600

    Update combinations.cpp

--- Recursion-&-Backtracking/combinations.cpp
+++ Recursion-&-Backtracking/combinations.cpp
@@ -23,3 +23,29 @@ public:
         return final_arr;
     }
 };
+
+class Solution {
+// i didnt realise i didnt need the set.
+public:
+    void kev(int i,int k,int n,vector<int> &ds,vector<vector<int>> &ans){
+        if(ds.size()==k){
+            ans.push_back(ds);
+            return;
+        }
+        for(int j = i; j <= n; j++ ){
+            ds.push_back(j);
+            kev(j+1,k,n,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> final_arr;
+        //set<vector<int>> ans;
+        vector<int> ds;
+        kev(1,k,n,ds,final_arr);
+        // for(auto s: ans){
+        //     final_arr.push_back(s);
+        // }
+        return final_arr;
+    }
+};
