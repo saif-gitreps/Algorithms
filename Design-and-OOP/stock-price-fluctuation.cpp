3cdc0bfe5e76a610ab19149d955a059ecaaca463
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-17 22:35:35 +0600

    Update stock-price-fluctuation.cpp

--- Design-and-OOP/stock-price-fluctuation.cpp
+++ Design-and-OOP/stock-price-fluctuation.cpp
@@ -54,3 +54,41 @@ public:
  * int param_3 = obj->maximum();
  * int param_4 = obj->minimum();
  */
+
+
+class StockPrice 
+// well i learned about set and multisert , today
+// which is the opposite of priority queue;
+public:
+    map<int, int> recent;
+    multiset<int> count;
+    StockPrice() {
+    }
+    void update(int timestamp, int price) {
+        if(recent.find(timestamp) != recent.end()){
+            count.erase(count.find(recent[timestamp]));
+        }
+        recent[timestamp] = price;
+        count.insert(price); 
+    }
+    
+    int current() {
+        return recent.rbegin()->second;
+    }
+    int maximum() {
+        return *count.rbegin();
+    }
+    
+    int minimum() {
+        return *count.begin();
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
