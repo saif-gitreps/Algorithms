1d0ada6b8881b858ea5f1036cdce4ed85a553b72
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-19 21:08:15 +0600

    Update Flower-planting-with-no-adjacent.cpp

--- Graph/Flower-planting-with-no-adjacent.cpp
+++ Graph/Flower-planting-with-no-adjacent.cpp
@@ -89,3 +89,56 @@ public:
         return valid_path;
     }
 };
+
+
+class Solution {
+//this worked till  7 test cases, not working for more.
+public:
+    vector<bool> visited;
+    vector<vector<int>> paths;
+    Solution(){
+        visited.resize(10000);
+        paths.resize(10000);
+    }
+    void bfs(int source,int n,vector<int> &valid_path){
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+        while(q.empty()==false){
+            int temp = q.front();
+            q.pop();
+            valid_path.push_back(temp);
+            if(valid_path.size()==n){
+                return;
+            }
+            for(auto s: paths[temp]){
+                if(!visited[s]){
+                    visited[s] = true;
+                    q.push(s);
+                }
+            }
+        }
+    }
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& a) {
+        vector<int> valid_path;
+        for(int i = 0 ; i < a.size(); i++ ){
+            paths[a[i][0]].push_back(a[i][1]);
+            paths[a[i][1]].push_back(a[i][0]);
+        }
+        bfs(1,n,valid_path);
+        if(valid_path.size()<n){
+            int existing_limit = valid_path.size();
+            int k = 0;
+            for(int i=0;i<n-existing_limit;i++){
+                if(k >= existing_limit){
+                    k = 0 ;
+                }
+                else{
+                    valid_path.push_back(valid_path[k]);
+                    ++k;
+                }
+            } 
+        }
+        return valid_path;
+    }
+};
