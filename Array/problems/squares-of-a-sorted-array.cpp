97cc3d1a4606ab4f4d812f10383e3395e7bfb16f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-19 21:44:57 +0600

    Update squares-of-a-sorted-array.cpp

--- Array/problems/squares-of-a-sorted-array.cpp
+++ Array/problems/squares-of-a-sorted-array.cpp
@@ -8,3 +8,25 @@ public:
         return nums;
     }
 };
+
+
+class Solution {
+// O(n) solution.
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int left = 0 ; int right = nums.size()-1;
+        int ptr = right;
+        while(left<=right){
+            if(abs(nums[right])>=abs(nums[left])){
+                ans[ptr--] = nums[right] * nums[right];
+                right--;  
+            }
+            else if(abs(nums[right])<abs(nums[left])){
+                ans[ptr--] = nums[left] * nums[left];
+                left++;
+            }
+        }
+        return ans;
+    }
+};
