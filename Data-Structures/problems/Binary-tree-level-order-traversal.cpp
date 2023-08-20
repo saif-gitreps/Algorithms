e6c2952621570c9e34bd042703208c35cc0ae95b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-20 15:12:32 +0600

    Update Binary-tree-level-order-traversal.cpp

--- Data-Structures/problems/Binary-tree-level-order-traversal.cpp
+++ Data-Structures/problems/Binary-tree-level-order-traversal.cpp
@@ -31,6 +31,50 @@ public:
                 q.push(temp->right);
             }
         }
+    }
+};
 
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+// had trouble bringing out the level for the trees , other than that i had similar concepts i guess.
+    //map<> visited(1000,false);
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans; 
+        if(root==NULL){
+            return ans;
+        }
+        //ans.push_back({root->val});
+        queue<TreeNode*> q;
+        q.push(root);
+        while(q.empty()==false){
+            vector<int> level;
+            int n = q.size();
+            for(int i = 0 ; i < n; i++){
+                TreeNode * temp = q.front();
+                q.pop();
+                if(temp->left!=NULL){
+                    q.push(temp->left);
+                }
+                if(temp->right!=NULL){
+                    q.push(temp->right);
+                }
+                level.push_back(temp->val);
+            }
+            ans.push_back(level);
+        }
+        return ans;
     }
 };
