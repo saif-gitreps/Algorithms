5f432e79314eca5b4041bd83a9195bb258d50bd4
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-22 00:19:12 +0600

    Create Rotate-Image.cpp

---
+++ Array/Rotate-Image.cpp
@@ -0,0 +1,17 @@
+class Solution {
+// first attempt , solved in 12 minutes, beats 32% in time , and sucked at memory
+public:
+    void rotate(vector<vector<int>>& a) {
+        vector<vector<int>> temp;
+        int col = a.size();
+        int row = a[0].size();
+        for(int j = 0; j < row; j ++){
+            vector<int> new_row;
+            for(int i = col-1;i>=0;i--){
+                new_row.push_back(a[i][j]);
+            }
+            temp.push_back(new_row);
+        }
+        a = temp;
+    }
+};
