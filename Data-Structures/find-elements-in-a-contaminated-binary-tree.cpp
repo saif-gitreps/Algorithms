08247d437040451c6bffc2c5c1152368ba0806d9
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-21 21:12:24 +0600

    Update find-elements-in-a-contaminated-binary-tree.cpp

--- Data-Structures/find-elements-in-a-contaminated-binary-tree.cpp
+++ Data-Structures/find-elements-in-a-contaminated-binary-tree.cpp
@@ -102,3 +102,55 @@ public:
  * FindElements* obj = new FindElements(root);
  * bool param_1 = obj->find(target);
  */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class FindElements {
+// i could have used the preorder method of searching , i just didnt know how to get the reference of the tree
+// lesson learned , i have to use dfs with tree
+public:
+    FindElements(TreeNode* root) : root(root) {
+        decontaminate(root, 0);
+    }
+
+    bool find(int target) {
+        return preorder_search(root, target);
+    }
+
+private:
+    TreeNode* root;
+
+    void decontaminate(TreeNode* node, int value) {
+        if (node) {
+            node->val = value;
+            decontaminate(node->left, 2 * value + 1);
+            decontaminate(node->right, 2 * value + 2);
+        }
+    }
+
+    bool preorder_search(TreeNode* node, int target) {
+        if (node) {
+            if (node->val == target) return true;
+            return preorder_search(node->left, target) || preorder_search(node->right, target);
+        }
+
+        return false;
+    }
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
