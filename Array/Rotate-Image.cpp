c6963caf619b0879bf39cebca7969233f2e9d13a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-22 18:27:11 +0600

    Update Rotate-Image.cpp

--- Array/Rotate-Image.cpp
+++ Array/Rotate-Image.cpp
@@ -15,3 +15,17 @@ public:
         a = temp;
     }
 };
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        for(int i = 0 ; i < a.size(); i++){
+            for(int j = 0 ; j < i; j++){
+                swap(a[i][j],a[j][i]);
+            }
+        }
+        for(int i =0 ; i < a.size(); i++ ){
+            reverse(a[i].begin(),a[i].end());
+        }
+    }
+};
