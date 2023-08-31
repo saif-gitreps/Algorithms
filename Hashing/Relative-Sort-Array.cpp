5eab550c9201b227075cee1ab281acafd7ee6244
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-31 19:28:25 +0600

    Update Relative-Sort-Array.cpp

--- Hashing/Relative-Sort-Array.cpp
+++ Hashing/Relative-Sort-Array.cpp
@@ -22,3 +22,32 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+//not best in terms of memeory and space but best i could come up with all these stress
+
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int,int> mp;
+        for(int i = 0 ; i < arr1.size(); i++){
+            mp[arr1[i]]++;
+        } 
+        vector<int> ans; 
+        for(int i = 0 ; i < arr2.size();i ++ ){
+            while(mp.count(arr2[i]) && mp[arr2[i]] > 0){
+                ans.push_back(arr2[i]);
+                mp[arr2[i]]--;
+            }
+        }
+        for(auto x : mp){
+            if(x.second>0){
+                int n = x.second;
+                while(n--){
+                    ans.push_back(x.first);
+                }
+            }
+        }
+        return ans;
+    }
+};
