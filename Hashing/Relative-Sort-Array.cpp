b7054eefac0ed350263e3f239ac7b75d0687ee45
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-08-31 19:14:55 +0600

    Create Relative-Sort-Array.cpp

---
+++ Hashing/Relative-Sort-Array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+// i did sort it and hash and i still got an error at test case 17.
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int,int> mp;
+        for(int i = 0 ; i < arr1.size(); i++){
+            mp[arr1[i]]++;
+        } 
+        vector<int> ans; 
+        for(int i = 0 ; i < arr2.size();i ++ ){
+            while(mp.count(arr2[i]) && mp[arr2[i]] != 0){
+                ans.push_back(arr2[i]);
+                mp[arr2[i]]--;
+            }
+        }
+        sort(arr1.begin(),arr1.end());
+        for(auto x : mp){
+            if(x.second!=0){
+                ans.push_back(x.first);
+            }
+        }
+        return ans;
+    }
+};
