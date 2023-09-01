024a518004bbf6112e5be507f9a1d37c66aefb78
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-01 10:56:23 +0600

    Create two-Sum-ii-Input-Array-Is-Sorted.cpp

---
+++ Array/two-Sum-ii-Input-Array-Is-Sorted.cpp
@@ -0,0 +1,20 @@
+class Solution {
+// suspicious how easy this solution is.
+public:
+    vector<int> twoSum(vector<int>& a, int target) {
+        int left = 0 ; 
+        int right = a.size()-1;
+        while(left < right){
+            if(a[left] + a[right] == target){
+                return {left+1 , right+1};
+            }
+            else if(a[left] + a[right] < target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {-1,-1};
+    }
+};
