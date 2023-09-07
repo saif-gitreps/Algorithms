9726c67856f0f8f7307f3708b90cea9854bce012
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-07 16:24:30 +0600

    Update Sum-Root-to-Leaf-Numbers.cpp

--- Data-Structures/Sum-Root-to-Leaf-Numbers.cpp
+++ Data-Structures/Sum-Root-to-Leaf-Numbers.cpp
@@ -38,3 +38,43 @@ public:
 
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+// same result but this is much much better in terms of space from O(n^2) to O(1).
+public:
+    int ans = 0;
+    void pre(TreeNode *root,int &ds){
+        if(root == NULL){
+            ans += ds;
+            return;
+        }
+        ds = ds*10 + (root->val);
+        pre(root->left,ds);
+        if(ds!=0){
+            ds = ds/10;
+        }
+        else{
+            ds = 0;
+        }
+        pre(root->right,ds);
+    }
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        int ds = 0;
+        pre(root,ds);
+        return ans;
+
+    }
+};
