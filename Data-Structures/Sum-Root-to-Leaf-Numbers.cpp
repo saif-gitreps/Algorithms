ffacaef2fbf1bc12da444a0f4aa52a4f2b87d9a4
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-07 15:47:11 +0600

    Create Sum-Root-to-Leaf-Numbers.cpp

---
+++ Data-Structures/Sum-Root-to-Leaf-Numbers.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+attempt 1 : idk why i cant debug whats wrong here.
+public:
+    vector<string> ans;
+    void pre(TreeNode *root,string &ds){
+        if(root == NULL){
+            ans.push_back(ds);
+            return;
+        }
+        ds.push_back(root->val + '0');
+        pre(root->left,ds);
+        ds.pop_back();
+        pre(root->right,ds);
+    }
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        string ds = "";
+        pre(root,ds);
+        for(int i= 0 ; i < ans.size(); i ++){
+            int num = 0;
+            for(int j = 0 ; j < ans[i].size(); j++){
+                num = (num * 10) + (ans[i][j] - '0');
+            }
+            sum += num;
+        }
+        return sum;
+
+    }
+};
