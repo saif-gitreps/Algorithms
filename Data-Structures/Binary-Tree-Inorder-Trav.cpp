88bb9c8fce99320252e4fa5d4db9174fc65530cd
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-07 10:00:09 +0600

    Create Binary-Tree-Inorder-Trav.cpp

---
+++ Data-Structures/Binary-Tree-Inorder-Trav.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   vector<int> v;
+   
+   void in(TreeNode* root){
+      if(root==NULL){
+         return;
+      }
+      in(root->left);
+      v.push_back(root->val);
+      in(root->right);
+   }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+       in(root);
+       return v;
+    }
+};
