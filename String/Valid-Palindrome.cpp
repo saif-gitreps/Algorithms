02b40da37af0e02f37f784ec489e77b27baa7dab
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-09 19:38:05 +0600

    Create Valid-Palindrome.cpp

---
+++ String/Valid-Palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+// attempt 1.
+public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for(int i = 0 ;i < s.size();i ++){
+          if(s[i] >= 'A' && s[i] <= 'Z'){
+            str.push_back(s[i] + 32 );
+          }
+          else if(s[i] >= 'a' && s[i] <= 'z'){
+            str += s[i];
+          }
+          else if(s[i] >= '0' && s[i] <= '9'){
+            str += s[i];
+          } 
+        }
+
+        int n = str.size();
+        for(int i = 0 ; i < str.size(); i++){
+          if(str[i] != str[n - i - 1]){
+            return false;
+          }
+        }
+        return true;
+    }
+};
