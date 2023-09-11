20f8326580fe37390b06716e905b74eae095722b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-11 19:39:58 +0600

    Update Valid-Sudoku.cpp

--- Array/Valid-Sudoku.cpp
+++ Array/Valid-Sudoku.cpp
@@ -43,3 +43,57 @@ public:
 
     }
 };
+
+
+
+class Solution {
+// FINALLYYY FIGURED THE BULLSHIT OUT MY SELF.
+public:
+    bool isValidSudoku(vector<vector<char>>& a) {
+        //solving row by row.
+        for(int i = 0 ; i < a.size() ; i ++){
+            unordered_map<char,int> mp;
+            for(int j = 0; j < a[0].size() ; j ++){
+                if(mp[a[i][j]] == 1 && a[i][j]!='.'){
+                    return false;
+                }
+                mp[a[i][j]] = 1;
+            }
+        }
+        //solving column by column.
+        for(int j = 0 ;j < a[0].size(); j++){
+            unordered_map<char,int> mp;
+            for(int i = 0; i < a.size(); i++){
+                if(mp[a[i][j]] == 1 && a[i][j]!='.'){
+                    return false;
+                }
+                mp[a[i][j]] = 1;
+            }
+        }
+        int row = 0;
+        int col = 0;
+        while(true){
+            unordered_map<char,int> mp;
+            if(col == 9){
+               row = row + 3;
+               col = 0;
+            }
+            if(row == 9){
+               break;
+            }  
+            for(int row_itr = row ; row_itr < row+3; row_itr ++){
+               for (int col_itr = col; col_itr < col+3; col_itr ++){
+                  if(a[row_itr][col_itr]!='.' && mp[a[row_itr][col_itr]] == 1){
+                      return false;
+                  }
+                  else{
+                    mp[a[row_itr][col_itr]] = 1;
+                  }
+               }
+            }
+            col = col + 3;   
+        }
+        return true;
+
+    }
+};
