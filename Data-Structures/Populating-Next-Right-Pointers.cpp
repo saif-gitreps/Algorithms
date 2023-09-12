de7824ee766061cfca8339a4447d9b9b71d9915c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-12 23:40:36 +0600

    Update Populating-Next-Right-Pointers.cpp

--- Data-Structures/Populating-Next-Right-Pointers.cpp
+++ Data-Structures/Populating-Next-Right-Pointers.cpp
@@ -91,3 +91,44 @@ class Solution {
         return root;
     }
 };
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+// this was the solution that i was dreaming of.
+public:
+    Node* connect(Node* root) {
+        if(root == NULL) {
+            return NULL;
+        }
+        if(root->left != NULL){
+            root->left->next = root->right;
+        }
+        // this line was the missing puzzle in my brain which is used 
+        // to connect the right most of a root with the left most node
+        // of another root.
+        if(root->right != NULL && root->next != NULL){
+            root->right->next = root->next->left;
+        }
+        root->left = connect(root->left);
+        root->right = connect(root->right);
+        return root;
+    }
+};
