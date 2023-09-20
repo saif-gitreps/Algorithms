679d685dac90108fbb9ef85a63b4b1b1238e39fb
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-20 12:00:50 +0600

    Update Count-Symmetric-Integers.cpp

--- MathAlg/Count-Symmetric-Integers.cpp
+++ MathAlg/Count-Symmetric-Integers.cpp
@@ -51,3 +51,26 @@ public:
         return count;
     }
 };
+
+class Solution {
+// OK so the original idea was correct ,we just had to ignorre all the odd lengthed numbers.
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for(int i = low; i <= high ; i++){
+            string num = to_string(i);
+            int sum1 = 0; int sum2 = 0;
+            if(num.size() % 2 != 0){
+                continue;
+            }
+            for(int j = 0; j < num.size()/2; j ++){
+                sum1 += num[j] - '0';
+                sum2 += num[num.size()-j-1] - '0';
+            }
+            if(sum1 == sum2 && sum1 != 0 && sum2 != 0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
