c94c897297daf522e864d409392663e6b939df71
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-21 21:19:20 +0600

    Update Compare-Version-Numbers.cpp

--- String/Compare-Version-Numbers.cpp
+++ String/Compare-Version-Numbers.cpp
@@ -26,3 +26,43 @@ public:
         return v1 == v2 ? 0 : (v1 > v2 ? 1 : -1);
     }
 };
+
+
+class Solution {
+// Elegant solution , self explanatory. 
+public:
+    int compareVersion(string ve1, string ve2) {
+   int i = 0;
+   int j = 0;
+   int n1 = ve1.size();
+   int n2 = ve2.size();
+
+   int num1 = 0;
+   int num2 = 0;
+
+   while(i < n1 || j < n2){
+      while( i < n1 && ve1[i] != '.'){
+         num1 = num1 * 10 + (ve1[i] - '0');
+         i ++ ;
+      }
+      while( j < n2 && ve2[j] != '.'){
+         num2 = num2 * 10 + (ve2[j] - '0');
+         j ++;
+      }
+
+      if(num1 > num2 ) {
+         return 1;
+      }
+      else if(num2 > num1 ){
+         return -1;
+      }
+      num1 = 0;
+      num2 = 0;
+      // why i ++ and j ++ , because , we stopped at a fullstop.
+      // next increment is other decimal digits.
+      i++;
+      j++;
+   }
+   return 0;           
+}
+};
