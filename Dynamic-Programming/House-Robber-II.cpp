8a6926bbaeb2cbac20bb05b71d64e857895b3135
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-26 17:50:45 +0600

    Update House-Robber-II.cpp

--- Dynamic-Programming/House-Robber-II.cpp
+++ Dynamic-Programming/House-Robber-II.cpp
@@ -26,3 +26,35 @@ public:
         return dp[n-1];
     }
 };
+
+
+class Solution {
+// getting stuck on edge cases all the time, this tine 51/75.
+public:
+    int kev(int n , vector<int> a, vector<int> &dp){
+        if(n == 0){
+            dp[n] = a[n];
+            return dp[n];
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int pick = a[n] + kev(n - 2, a, dp);
+        int not_pick = kev(n - 1, a, dp);
+        if(n == a.size() - 1) {
+            int min_to_delete = min(a[a.size()-1],a[0]);
+            pick = abs(pick - min_to_delete);
+        }
+        dp[n] = max(pick, not_pick);
+        return dp[n];
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        kev(n - 1, nums, dp);
+        return dp[n-1];
+    }
+};
