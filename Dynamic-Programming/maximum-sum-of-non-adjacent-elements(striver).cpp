bde0e2f8bb8ce96f0a9dbb70c8f39cd2105888ae
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-26 14:24:56 +0600

    Update maximum-sum-of-non-adjacent-elements(striver).cpp

--- Dynamic-Programming/maximum-sum-of-non-adjacent-elements(striver).cpp
+++ Dynamic-Programming/maximum-sum-of-non-adjacent-elements(striver).cpp
@@ -20,9 +20,52 @@ int kev(int n , vector<int> a, vector<int> &dp){
     return dp[n]; 
     
 }
-
 int maximumNonAdjacentSum(vector<int> &nums){
     vector<int> dp(nums.size(),-1);
     kev(nums.size()-1, nums, dp);
     return dp[nums.size()-1];
+}
+
+//===================================================================================
+tablution method.
+
+#include <bits/stdc++.h>
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    vector<int> dp(nums.size(),-1);
+    int n=nums.size();
+    dp[0]=nums[0];
+    for(int i=1;i<n;i++)
+    {
+        int take = nums[i]; 
+        if(i>1)
+        take += dp[i-2];
+        int notTake = dp[i-1];
+
+        dp[i]=max(take,notTake);
+    }
+    return dp[n-1];
+}
+//======================================================================
+
+space optimized method.
+
+#include <bits/stdc++.h>
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    vector<int> dp(nums.size(),-1);
+    int n=nums.size();
+    dp[0]=nums[0];
+    int curr = nums[0];
+    int prev = 0;
+    for(int i=1;i<n;i++)
+    {
+        int take = nums[i]; 
+        if(i>1)
+        take += prev;
+        int notTake = curr;
+        prev = curr;
+        curr=max(take,notTake);
+    }
+    return curr;
 }
