9bda656acc3291b145f92a266136a7de2457934c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-09-28 20:15:31 +0600

    Update Minimum-speed-to-arrive-on-Time.cpp

--- Binary-Search/Minimum-speed-to-arrive-on-Time.cpp
+++ Binary-Search/Minimum-speed-to-arrive-on-Time.cpp
@@ -26,3 +26,31 @@ public:
         return right;
     }
 };
+
+
+class Solution {
+// similar code , i think the return statement had issues.
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int right  = 10000000, left = 1;
+        long long int mid;
+        if (hour < dist.size() - 1) {
+            return -1;
+        }
+        while(left <= right){
+            mid = left + (right-left)/2;
+            double total_hour = 0;
+            for(int i = 0 ; i < dist.size(); i++){
+                total_hour = ceil(total_hour);
+                total_hour += ((double)dist[i] / mid);
+            }
+            if (total_hour > hour) {
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return (left > 10000000) ? -1 : left;
+    }
+};
