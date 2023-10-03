c29e1b64de1e31999dc71a2aa76092ef6ca7f3c4
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 22:38:10 +0600

    Update Maximum-Subarray.cpp

--- Dynamic-Programming/Maximum-Subarray.cpp
+++ Dynamic-Programming/Maximum-Subarray.cpp
@@ -1,5 +1,5 @@
 class Solution {
-// I knew this algorithm from before.
+// I knew this algorithm from before , so This algorithm is called Kadane's algorithm.
 public:
     int maxSubArray(vector<int>& nums) {
         if(nums.size() == 1){
@@ -17,5 +17,31 @@ public:
             mx = max(sum, mx);
         }
         return mx;
+    }
+};
+
+
+class Solution {
+// idk this dp approach isnt really the right answe but i tried.
+public:
+    int kev(int n, vector<int> a, vector<int> &dp){
+        if(n == 0){
+            return dp[n] = a[0];
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        int pick = kev(n-1, a, dp) + a[n];
+        int not_pick = kev(n-2, a, dp);
+        return dp[n] = max(pick, not_pick); 
+    }
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        kev(nums.size()-1 , nums, dp);
+        return dp[nums.size()-1];
+
     }
 };
