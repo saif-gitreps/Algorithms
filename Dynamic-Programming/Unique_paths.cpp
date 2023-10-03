9f0f3319049a11e5005dbaa39832e2656ae7c60c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 13:05:30 +0600

    Update Unique_paths.cpp

--- Dynamic-Programming/Unique_paths.cpp
+++ Dynamic-Programming/Unique_paths.cpp
@@ -1,48 +1,3 @@
-#include <bits/stdc++.h>
-// attempt1: getting overflow.
-using namespace std;
-#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
-#define GGG 9223372036854775807
-#define pb push_back
-#define FAST(SUIII); ios_base::sync_with_stdio(false);cin.tie(NULL);
-const int MAXN = 100010;
-
-void kev(int m,int n,int row,int col,int &ans){
-   if(m==row && n==col){
-      ans += 1;
-      return;
-   }
-   if(m<row){
-      m += 1;
-      kev(m,n,row,col,ans);
-      m -= 1;
-   }
-   if(n<col){
-      n += 1;
-      kev(m,n,row,col,ans);
-      n -= 1;
-   }
-   if(m<row && n<col){
-      n++;
-      kev(m,n,row,col,ans);
-      n--;
-   }
-   if(n<col && m<row){
-      m++;
-      kev(m,n,row,col,ans);
-      m--;
-   }
-}  
-
-int main(){
-   int m;int n;
-   cin>>m>>n;
-   int ans =0;
-   map<pair<int,int>,bool> mp;
-   kev(0,0,m,n,ans);
-   cout<<ans;
-}
-
 class Solution {
 // holyy sheet this worked, but it shows TLE, worked till 50 cases.
 public:
@@ -131,4 +86,24 @@ public:
         kev(m,n,dp);
         return dp[m][n];
     }
+
+class Solution {
+// tabulation method .
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i = 0; i < m ;i ++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1; continue;
+                }
+                // basically i am storying up + left in dp[i][j].
+                dp[i][j] = (i > 0? dp[i-1][j] : 0)
+                        +  (j > 0? dp[i][j-1] : 0);
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
 };
