bd05f2f1d00b1831153d070099056ac03c345270
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 17:37:26 +0600

    Update Linked-List-cycle-II.cpp

--- Data-Structures/Linked-Linst/Linked-List-cycle-II.cpp
+++ Data-Structures/Linked-Linst/Linked-List-cycle-II.cpp
@@ -14,3 +14,36 @@ public:
         return NULL;
     }
 };
+
+
+
+class Solution {
+// Clever solution . just simulate it and see , i need to use this method, fast and slow method.
+// basically we wait till fast and slow meets , when  they meet , we run another loop till slow and entry meets, then return that meeting
+// point of entry and slow.
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return NULL;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
+
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast)
+            {
+                while(entry != slow)
+                {
+                    slow = slow->next;
+                    entry = entry->next;
+                }
+                return entry;
+            }
+        }
+         return NULL;
+    }
+};
