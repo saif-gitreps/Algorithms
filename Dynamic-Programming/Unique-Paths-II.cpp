9c27664a1977dd515951059b4de0b88121af8176
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 23:46:30 +0600

    Update Unique-Paths-II.cpp

--- Dynamic-Programming/Unique-Paths-II.cpp
+++ Dynamic-Programming/Unique-Paths-II.cpp
@@ -26,3 +26,38 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+// Top - down appraoch worked .
+public:
+    int kev(int row,int col, vector<vector<int>>& og, vector<vector<int>>&dp){
+        if(row == 0 && col == 0){
+            if(og[row][col] == 1){
+                return dp[row][col] = 0;
+            }
+            return dp[row][col] = 1;
+            
+        }
+        if(row < 0 || col < 0){
+            return 0;
+        }
+        if(og[row][col] == 1){
+            return dp[row][col] = 0;
+        }
+        if(dp[row][col] != -1){
+            return dp[row][col];
+        }
+        int up = 0,left = 0;
+        up = kev(row -1, col, og, dp);
+        left = kev(row, col-1, og, dp) ;
+        return dp[row][col] = up + left;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& og) {
+        int row = og.size();
+        int col = og[0].size();
+        vector<vector<int>> dp(row,vector<int> (col,-1));
+        kev(row-1 , col-1 , og ,dp);
+        return dp[row-1][col-1];
+    }
+};
