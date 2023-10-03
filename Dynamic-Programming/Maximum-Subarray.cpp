6ff4430518adcef8c8fd20c81f3b1d177889a7c5
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 22:44:30 +0600

    Update Maximum-Subarray.cpp

--- Dynamic-Programming/Maximum-Subarray.cpp
+++ Dynamic-Programming/Maximum-Subarray.cpp
@@ -42,6 +42,32 @@ public:
         vector<int> dp(nums.size(), -1);
         kev(nums.size()-1 , nums, dp);
         return dp[nums.size()-1];
+    }
+};
 
+
+class Solution {
+// divide and conquer solution.
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSub(nums, 0, nums.size()-1);
+    }
+private:
+    int maxSub(vector<int>& nums, int l, int r) {
+        if (l > r)
+            return INT_MIN;
+        int mid = (r-l)/2+l;
+        int lMax = maxSub(nums, l, mid-1);
+        int rMax = maxSub(nums, mid+1, r);
+        int s1 = 0, s2 = 0;
+        for (int i = mid-1, cur = 0; i >= l; i--) {
+            cur += nums[i];
+            s1 = max(s1, cur);
+        }
+        for (int i = mid+1, cur = 0; i <= r; i++) {
+            cur += nums[i];
+            s2 = max(s2, cur);
+        }
+        return max(s1+s2+nums[mid], max(lMax, rMax));
     }
 };
