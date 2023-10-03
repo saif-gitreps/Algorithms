83d0950cb591fbf00694e564ff475708877b0898
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-04 01:03:57 +0600

    Update and rename Unique-Paths-II.cpp to Unique-paths-II.cpp

--- Dynamic-Programming/Unique-paths-II.cpp
+++ Dynamic-Programming/Unique-paths-II.cpp
@@ -61,3 +61,35 @@ public:
         return dp[row-1][col-1];
     }
 };
+
+class Solution {
+// tab method worked
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& og) {
+    int row = og.size();
+    int col = og[0].size();
+    vector<vector<int>> dp(row, vector<int>(col, 0));
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (og[i][j] == 1) {
+                dp[i][j] = 0;
+            } 
+            else if (i == 0 && j == 0) {
+                dp[i][j] = 1;
+            } 
+            else {
+                int up = 0, left = 0;
+                if (i >= 1) {
+                    up = dp[i - 1][j];
+                }
+                if (j >= 1) {
+                    left = dp[i][j - 1];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+    }
+    return dp[row-1][col-1];
+    }
+};
