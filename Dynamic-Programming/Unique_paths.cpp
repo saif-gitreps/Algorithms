5156201dac30dec2d30077a4327e2a09ae8e1e09
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-03 17:11:18 +0600

    Update Unique_paths.cpp

--- Dynamic-Programming/Unique_paths.cpp
+++ Dynamic-Programming/Unique_paths.cpp
@@ -87,6 +87,7 @@ public:
         return dp[m][n];
     }
 
+};
 class Solution {
 // tabulation method .
 public:
@@ -106,4 +107,38 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+
+//==========================================================================
+//combinatoics solution:
+// idea is that we will always take m-1 direction and m-1 direction, means if col is 3 , 
+// we will definitely have to move 2 steps from top-left.
+// so m-1  + n -1 = (m + n - 2) is the total number of directions i can choose.
+// so we have combinatics forula which is , number<Cn> = (number-2 x number-1 x number) / (n x n-1 x n-2) 
+// And we apply this formula in terms of our ans that is TotalDirection<C col-1> || TotalDurection<C row-1>
+// For example row=2,col=3, if TotalDirection is (2 + 3 -2) = 3.
+// applying => 3<col-1>  =  (1 x 2 x 3) / (2 x 1) = 3 , which is correct and
+// "Combinations" formula, 
+// and it's often denoted as "C(n, k)" or "n choose k." Its formal representation is:
+
+// C(n, k) = n! / (k! * (n - k)!)
+
+// Where:
+
+// "n" is the total number of items to choose from.
+// "k" is the number of items to choose.
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int number = m + n - 2;
+        int Cr = min(m-1,n-1);
+
+        double ans = 1;
+        for(int i = 1; i <= Cr; i++){
+            ans = ans * (number - Cr + i) / i;
+        }
+        return (int)ans;
+    }
 };
+
