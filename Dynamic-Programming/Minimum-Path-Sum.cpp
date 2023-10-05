2f928e6e475517707f1973ace7377f1e741bd9ac
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-05 12:44:31 +0600

    Update Minimum-Path-Sum.cpp

--- Dynamic-Programming/Minimum-Path-Sum.cpp
+++ Dynamic-Programming/Minimum-Path-Sum.cpp
@@ -24,3 +24,30 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+class solutuon{
+// successfully did tabulation as well. 
+public:
+  int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(); int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int> (n, -1));
+        for(int i = 0 ; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }
+                else{
+                    int up = 100000; int left = 100000; 
+                    if(i > 0){
+                        up = dp[i-1][j] + grid[i][j];
+                    }
+                    if(j > 0){
+                        left = dp[i][j-1] + grid[i][j];
+                    }
+                    dp[i][j] = min(up , left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
