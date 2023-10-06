aaae180032196441fa932d44e705eef88e845727
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-06 20:02:00 +0600

    Update Stickers-To-Spell-Word.cpp

--- Dynamic-Programming/Stickers-To-Spell-Word.cpp
+++ Dynamic-Programming/Stickers-To-Spell-Word.cpp
@@ -31,3 +31,47 @@ public:
         return total_count == 0? -1 : total_count;
     }
 };
+
+
+class Solution {
+public:
+    int kev(string target, vector<vector<int>> mp, unordered_map<string,int> &dp){
+        if(dp.count(target)){
+            return dp[target];
+        }
+        int n = mp.size();
+        int ans = INT_MAX;
+        vector<int> mini(26,0);
+        for(int i = 0 ; i < target.size(); i++){
+            mini[target[i] - 'a']++;
+        }
+        for(int i = 0; i < n; i++){
+            if(mp[i][target[0] - 'a'] == 0){
+                continue;
+            }
+            string s;
+            for(int j = 0; j < 26; j++){
+                if(mini[j] - mp[i][j] > 0){
+                    s += string(mini[j] - mp[i][j], 'a' + j);
+                }
+            }
+            int tmp = kev(s, mp, dp);
+            if( tmp != -1){
+                ans = min(ans, 1 + tmp);
+            }
+        }
+        return dp[target] = ans == INT_MAX ? -1 : ans;
+    }
+    int minStickers(vector<string>& a, string target) {
+        int n = a.size();
+        vector<vector<int>> mp(a.size(), vector<int>(26,0));
+        unordered_map<string, int> dp;
+        for(int i = 0 ; i < a.size(); i++){
+            for(int j = 0 ; j < a[i].size(); j++){
+                mp[i][a[i][j] - 'a']++;
+            }
+        }
+        dp[""] = 0;
+        return kev(target, mp, dp);
+    }
+};
