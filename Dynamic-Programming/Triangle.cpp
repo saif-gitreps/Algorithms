71414182bfafa4dc5f370204dad902f209d1d747
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-07 12:35:10 +0600

    Update Triangle.cpp

--- Dynamic-Programming/Triangle.cpp
+++ Dynamic-Programming/Triangle.cpp
@@ -52,3 +52,41 @@ public:
         return kev(0, 0, triangle, dp);
     }
 };
+
+
+class Solution {
+// tabulation rule of thumb , if your recurrence was top-down , then your tabluation will be bottom-up. rest is copy paste.
+public:
+    int kev(int i,int j, vector<vector<int>> &a, vector<vector<int>> &dp){
+        if(i == a.size() -1 ){
+            return dp[i][j] = a[i][j];
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int fs = INT_MAX;
+        int ss = INT_MAX;
+        fs = a[i][j] + kev(i+1, j, a, dp);
+        ss = a[i][j] + kev(i+1, j+1, a, dp);
+        return dp[i][j] = min(fs, ss);
+    }
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[n - 1].size();
+        vector<vector<int>> dp(n , vector<int> (n, -1));
+        for(int j = 0 ; j < n ; j ++){
+            dp[n-1][j] = a[n-1][j];
+        }
+        for(int i = n-2 ; i >= 0; i--){
+            for(int j = i; j >= 0 ; j--){
+                // now jus copy paste my kev.
+                int fs = INT_MAX;
+                int ss = INT_MAX;
+                fs = a[i][j] + dp[i+1][j];
+                ss = a[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(fs , ss);
+            }
+        }
+        return dp[0][0];
+    }
+};
