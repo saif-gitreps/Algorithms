a5207e8c9e6045c6c9ba98f3e851ae6f53849df5
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-08 21:02:55 +0600

    Update Valid-Triangle-Number.cpp

--- Binary-Search/Valid-Triangle-Number.cpp
+++ Binary-Search/Valid-Triangle-Number.cpp
@@ -20,3 +20,32 @@ public:
     return count;
     }
 };
+
+class Solution {
+// two pointer sorting ,on the principle that in a sorted array , a b c , if a+b> c , then no need to check a+c or b+c .
+// using this idea we can do 2 pointer .
+// sorting Nlog N, loop , TC -> O(n^2 log N).
+public:
+    int triangleNumber(vector<int>& nums) {
+        if(nums.size() < 3){
+            return 0;
+        }
+        int count = 0;
+        sort(nums.begin(),nums.end());
+        // i >= 2 because i want to go till third first element from the back.
+        for(int i = nums.size()-1 ; i >= 2; i--){
+            int left = 0;
+            int right = i - 1;
+            while(left < right){
+                if(nums[left] + nums[right] > nums[i]){
+                    count += (right - left);
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return count;
+    }
+};
