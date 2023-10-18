539e3399c4a57ab9ca561c272e7645e6cd3f79f1
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-18 23:49:33 +0600

    Update Cherry-Pickup-II.cpp

--- Dynamic-Programming/Cherry-Pickup-II.cpp
+++ Dynamic-Programming/Cherry-Pickup-II.cpp
@@ -119,3 +119,46 @@ public:
 
     }
 };
+
+
+
+class Solution {
+// note: this is not my code but look at this ridiculously complicatted code.
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int dp[n][m][m];
+        memset(dp,0,sizeof(dp));
+        int ans=0;
+        dp[0][0][m-1]=grid[0][0]+grid[0][m-1];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k=0;k<m;k++){
+                    if(j>i || k<m-i-1) continue;
+                    if(j>k) continue;
+                    dp[i][j][k]=dp[i-1][j][k];
+                    if(j-1>=0){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k]);
+                        if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k-1]);
+                        if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j-1][k+1]);
+                    }
+                    if(j+1<m){
+                        dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k]);
+                        if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k-1]);
+                        if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j+1][k+1]);                        
+                    }
+                    if(k-1>=0) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j][k-1]);
+                    if(k+1<m) dp[i][j][k]=max(dp[i][j][k],dp[i-1][j][k+1]);
+                    if(j!=k){
+                        dp[i][j][k]+=(grid[i][j]+grid[i][k]);
+                    }else{
+                        dp[i][j][k]+=grid[i][j];
+                    }
+                    ans=max(ans,dp[i][j][k]);
+                }
+            }
+        }
+        return ans;
+    }
+};
