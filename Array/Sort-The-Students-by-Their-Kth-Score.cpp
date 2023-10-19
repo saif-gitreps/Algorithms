569676793aa1e5a8f7dd622afda108f3ad293530
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-19 22:46:31 +0600

    Update Sort-The-Students-by-Their-Kth-Score.cpp

--- Array/Sort-The-Students-by-Their-Kth-Score.cpp
+++ Array/Sort-The-Students-by-Their-Kth-Score.cpp
@@ -15,3 +15,38 @@ public:
         return a;
     }
 };
+
+
+class Solution {
+// HAHA just implemented quick sort logic , it worked. SUI.
+public:
+    int party_boy(vector<vector<int>>& a, int k,int start, int end){
+        int pivot = a[end][k];
+        int i=start,j=end;
+        int pindex = start;
+        for(int i = start; i < end; i++){
+            if(a[i][k] > pivot){
+                vector<int> temp = a[i];
+                a[i] = a[pindex];
+                a[pindex] = temp; 
+                pindex++;
+            }
+        }
+        vector<int> temp = a[pindex];
+        a[pindex] = a[end];
+        a[end] = temp;
+        return pindex;
+    }
+
+    void quicky_boy(vector<vector<int>>& a, int k, int left,int right){
+        if(left < right){
+            int partition_index = party_boy(a, k, left, right);
+            quicky_boy(a, k, left, partition_index - 1);
+            quicky_boy(a, k, partition_index + 1, right);    
+        }
+    }
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& a, int k) {
+        quicky_boy(a, k , 0, a.size()-1);
+        return a;
+    }
+};
