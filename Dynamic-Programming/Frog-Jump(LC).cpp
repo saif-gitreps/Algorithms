f0a8158f5224d166aaeb35dffd35cbc2669ccc1f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-27 17:29:46 +0600

    Update Frog-Jump(LC).cpp

--- Dynamic-Programming/Frog-Jump(LC).cpp
+++ Dynamic-Programming/Frog-Jump(LC).cpp
@@ -70,3 +70,41 @@ public:
         return kev(0, 0, stones);
     }
 };
+
+class Solution {
+// YAS BAYBE <3 , first ever hard problem solved on my own.
+// so 1D dp array didnt work because , you see , theres many K-values by which you can reach a stone.
+// example , you can reach a stone with a[I] + K , or a[I-1] + k + 1, so there is different stones with 
+// different K values with which you can reach so for that i had to use 2D dp array.
+public:
+    bool kev(int i,int k, vector<int> a, vector<vector<int>> &dp){
+        // if(i >= a.size()){
+        //     return false;
+        // }
+        if(i == a.size()-1){
+            return dp[i][k] = true;
+        }
+        if(dp[i][k] != -1){
+            return dp[i][k];
+        }
+        int x = false;
+        int y = false;
+        int z = false;
+        for(int j = i+1; j < a.size(); j++){
+            if(k + a[i] == a[j]){
+                x = kev(j , k , a, dp);
+            }
+            if((k+1) + a[i] == a[j]){
+                y = kev(j , k + 1 , a, dp);
+            }
+            if((k-1) + a[i] == a[j]){
+                z = kev(j , (k - 1) , a, dp);
+            }
+        }
+        return dp[i][k] = (x || y || z);
+    }
+    bool canCross(vector<int>& stones) {
+        vector<vector<int>> dp(stones.size(),vector<int> (stones.size(),-1));
+        return kev(0, 0, stones, dp);
+    }
+};
