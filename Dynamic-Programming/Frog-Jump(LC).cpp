c0ef53d2659e9d2230d2998ba71afdbf5f97f0d6
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-27 16:55:26 +0600

    Update Frog-Jump(LC).cpp

--- Dynamic-Programming/Frog-Jump(LC).cpp
+++ Dynamic-Programming/Frog-Jump(LC).cpp
@@ -31,3 +31,40 @@ public:
         return kev(1, 1, stones);
     }
 };
+
+
+class Solution {
+// 14/50 now , i think this will go on from test case to test case.
+public:
+    bool kev(int i,int k, vector<int> a){
+        if(i >= a.size()){
+            return false;
+        }
+        if(i == a.size()-1){
+            // if(k + a[i-1] == a[i] ||
+            //   (k+1) + a[i-1] == a[i] ||
+            //   (k-1) + a[i-1] == a[i] ){
+            //     return true;
+            // }
+            return true;
+        }
+        int x = false;
+        int y = false;
+        int z = false;
+        for(int j = i+1; j < a.size(); j++){
+            if(k + a[i] == a[j]){
+                x = kev(j , k , a);
+            }
+            if((k+1) + a[i] == a[j]){
+                y = kev(j , k + 1 , a);
+            }
+            if((k-1) + a[i] == a[j]){
+                y = kev(j , (k - 1) , a);
+            }
+        }
+        return (x || y || z);
+    }
+    bool canCross(vector<int>& stones) {
+        return kev(0, 0, stones);
+    }
+};
