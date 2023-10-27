39462a5f0bf9b1438c53775e6ed537403c65f279
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-27 11:50:25 +0600

    Update 132-Pattern.cpp

--- Greedy/132-Pattern.cpp
+++ Greedy/132-Pattern.cpp
@@ -19,3 +19,31 @@ public:
         return false;
     }
 };
+
+class Solution {
+// solution with mono tonic stack, we store the elements and the minimum that was found.
+// so we are storing 2 variables at the same time. if we find a current element
+// that is smaller than the element at top(the max element since this is a invserve sorted stack),
+// And then we check if the minimum element found is also smaller than the current element at the iteration, then we can return true.
+
+public:
+    bool find132pattern(vector<int>& nums) {
+        if(nums.size() < 3){
+            return false;
+        }
+        int mn = nums[0];
+        stack<pair<int,int>> s;
+        //Mono tonic stack is like sorted stack. 
+        for(int i =  1; i < nums.size(); i++){
+            while(s.size() >= 1 && s.top().first <= nums[i]){
+                s.pop();
+            }
+            if(s.size() >= 1 && nums[i] < s.top().first && nums[i] > s.top().second){
+                return true;
+            }
+            s.push({nums[i], mn});
+            mn = min(mn, nums[i]);
+        }
+        return false;
+    }
+};
