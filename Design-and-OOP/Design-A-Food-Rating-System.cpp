f54d4c8bd46e9b4ac4d29565f039d8b0e81f875b
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-10-28 21:34:24 +0600

    Update Design-A-Food-Rating-System.cpp

--- Design-and-OOP/Design-A-Food-Rating-System.cpp
+++ Design-and-OOP/Design-A-Food-Rating-System.cpp
@@ -32,3 +32,48 @@ public:
  * obj->changeRating(food,newRating);
  * string param_2 = obj->highestRated(cuisine);
  */
+
+
+class FoodRatings {
+//2nd attempt same result
+public:
+    map<string,int> cuisMaxMap;
+    map<string,string> foodCuisMap;
+    map<string,int> food;
+    FoodRatings(vector<string>& foods, vector<string>& c, vector<int>& rate) {
+        for(int i = 0; i < c.size(); i++){
+            cuisMaxMap[c[i]] = max(cuisMaxMap[c[i]] , rate[i]);
+            foodCuisMap[foods[i]] = c[i];
+            food[foods[i]] = rate[i]; 
+        }
+    }
+    void changeRating(string checkFood, int newRating) {
+        food[checkFood] = newRating;
+        if(cuisMaxMap[foodCuisMap[checkFood]] == food[checkFood]){
+            cuisMaxMap[foodCuisMap[checkFood]] = 0;
+            for(auto s : food){
+               cuisMaxMap[foodCuisMap[checkFood]] = 
+               max(cuisMaxMap[foodCuisMap[checkFood]] , s.second);   
+            }
+        }
+        else{
+           cuisMaxMap[foodCuisMap[checkFood]] = 
+           max(cuisMaxMap[foodCuisMap[checkFood]] , newRating);
+        } 
+    }
+    string highestRated(string cuisine) {
+        for(auto s : food){
+            if(s.second == cuisMaxMap[cuisine]){
+                return s.first;
+            }
+        }
+        return "";
+    }
+};
+
+/**
+ * Your FoodRatings object will be instantiated and called as such:
+ * FoodRatings* obj = new FoodRatings(foods, cuisines, ratings);
+ * obj->changeRating(food,newRating);
+ * string param_2 = obj->highestRated(cuisine);
+ */
