2f7bb9a8606c1c3553b6ade2210fac4c76e4f2e4
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-01 22:31:33 +0600

    Create Brick-Wall.cpp

---
+++ Hashing/Brick-Wall.cpp
@@ -0,0 +1,27 @@
+class Solution {
+// The intuition is interesting.
+// We bascially check all the gaps, and then then we increment it,
+// After that we take the maximum gap there is and then -= the size or the number of rows.
+// Why is that because , when you think about it , the more gaps we find in each row , the least brick we have to cut.
+// If on the 4th gap we consistently get 5 gaps from 7 rows of brick ,then we only have to cut 2 bricks if we use the Gap number 4.
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i < wall.size(); i++){
+            int gap = 0;
+            for(int j = 0; j < wall[i].size()-1; j++){
+                gap += wall[i][j];
+                mp[gap]++;
+            }
+        }
+        int mx = 0;
+        int mx_gap = 0;
+        for(auto item: mp){
+            if(item.second > mx){
+                mx_gap = item.first;
+                mx = item.second;
+            }
+        }
+        return wall.size() - mx;    
+    }
+};
