a73daae2e0ad3426c47ebe057465b80683f43fe9
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-05 23:38:59 +0600

    Update Bulls-And-Cows.cpp

--- Hashing/Bulls-And-Cows.cpp
+++ Hashing/Bulls-And-Cows.cpp
@@ -25,3 +25,35 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    string getHint(string s, string g) {
+        int cows = 0;
+        int bulls = 0;
+        unordered_map<char,int> mp;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == g[i]){
+                bulls++;
+            }
+            else
+                // so here we are saying if s[i] is negative that means it appeared somewhere in the wrong direction.
+                // numbers[secret.charAt(i)-'0'] is negative only if this character appeared in the guess more times 
+                // then in the secret which means that this character in the 
+                // secret can be matched with one of the previous characters in the guess.
+                if(mp[s[i]] < 0){
+                    cows++;
+                }
+                if(mp[g[i]] > 0){
+                    cows++;
+                }
+                mp[s[i]] ++;
+                mp[g[i]] --;
+            }
+        }
+        
+        return to_string(bulls) + "A" + to_string(cows) + "B";
+
+    }
+};
