f344754cf06bfd136d56b425550e4b4ab0a1c411
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-12 22:44:45 +0600

    Update Path-Sum-III.cpp

--- Data-Structures/Path-Sum-III.cpp
+++ Data-Structures/Path-Sum-III.cpp
@@ -20,3 +20,38 @@ public:
         return kev(root, targetSum, 0);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    void kev(TreeNode * root, int ts, long long sum){
+        if(root == NULL){
+            return;
+        }
+        if(sum + root->val == ts){
+            ans++;
+        }
+        kev(root->left, ts, sum + root->val);
+        kev(root->right, ts, sum + root->val);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root != NULL){
+            kev(root, targetSum, 0);
+            pathSum(root->left,targetSum);
+            pathSum(root->right,targetSum);
+        }
+        return ans;
+    }
+};
