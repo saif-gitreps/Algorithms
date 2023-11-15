3419f5b82113757d674a4371384015d15164ce65
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-15 12:09:09 +0600

    Update and rename Array-partition-with-minimum-difference.cpp to Array-partition-with-minimum-difference(striver).cpp

---
+++ Dynamic-Programming/Array-partition-with-minimum-difference(striver).cpp
@@ -0,0 +1,26 @@
+// 45/50 cases passed.
+// update: 49/ 50 cases passed.
+int kev(int n, int diff, long long sum,vector<int> a, vector<vector<int>> &dp){
+	if(n < 0){
+		return INT_MAX;
+	}
+	if(n == 0){
+		return min(abs((sum - (diff + a[n])) - (diff + a[n])), 
+		abs((sum - diff) - diff));
+	}
+	if(dp[n][diff] != -1){
+		return dp[n][diff];
+	}
+	int pick = INT_MAX,not_pick = INT_MAX;
+	pick = kev(n-1, diff + a[n], sum, a, dp);
+	not_pick = kev(n-1, diff, sum ,a ,dp);
+	return dp[n][diff] = min(pick, not_pick);
+}
+int minSubsetSumDifference(vector<int>& a, int n){
+	long long sum = 0;
+	for(int i = 0 ; i < n; i++){
+		sum += a[i];
+	}
+	vector<vector<int>> dp(n, vector<int> (sum, -1));
+	return kev(n - 1 , 0, sum, a, dp);
+}
