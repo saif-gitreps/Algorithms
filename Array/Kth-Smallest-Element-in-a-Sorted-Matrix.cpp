c893e3d01e06040b29373fa1aec3a13af37f4534
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-17 12:19:32 +0600

    Update Kth-Smallest-Element-in-a-Sorted-Matrix.cpp

--- Array/Kth-Smallest-Element-in-a-Sorted-Matrix.cpp
+++ Array/Kth-Smallest-Element-in-a-Sorted-Matrix.cpp
@@ -17,3 +17,21 @@ public:
         return *it;
     }
 };
+
+
+class Solution {
+// using maxheap or priority queue.
+public:
+    int kthSmallest(vector<vector<int>>& a, int k) {
+        priority_queue<int> pq;
+        for(int i = 0 ; i < a.size(); i++){
+            for(int j = 0; j < a[0].size(); j++){
+                pq.push(a[i][j]);
+                if(pq.size() > k){
+                    pq.pop();
+                }
+            }
+        }
+        return pq.top();
+    }
+};
