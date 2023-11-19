810d578d595098208f86c63f9a5928f1edfba633
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-19 16:55:31 +0600

    Update partition-to-k-equal-sum-subsets.cpp

--- Dynamic-Programming/partition-to-k-equal-sum-subsets.cpp
+++ Dynamic-Programming/partition-to-k-equal-sum-subsets.cpp
@@ -40,3 +40,49 @@ public:
         return ans >= k ? true : false;
     }
 };
+
+
+class Solution {
+// working solution, so here what our intuition is:
+// First we check if we can take the sum of the array and divide it by K, and does it give us reminder 0 or no.
+// If it does then we may or may not be able to divide the array into K groups with equal sums.
+// Our recursion will start from 0... N and throughout the traversal it will pick up sum, if our sum == total_sum/k 
+// then we wil go back to the first index but this time , the elements that were picked in the previous recurison that gave the result
+// will be hashed so they wont be allowed to be picked again.
+public:
+    bool kev(int i, int k,int sum, int target,vector<int> &a, vector<int> &dp){
+        if(i >= a.size()) {
+            return false;
+        }
+        if(k == 1){
+            return true;
+        }
+        if(sum == target){
+            return kev(0, k-1, 0, target, a, dp);
+        }
+        for(int j = i; j < a.size(); j++){
+            if(dp[j] == 1 || sum + a[j] > target){
+                continue;
+            }
+            dp[j] = 1;
+            if(kev(j + 1, k, sum + a[j], target, a, dp)) {
+                return true;          
+            }
+            dp[j] = -1;
+        }
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i = 0 ; i < n ; i ++){
+            sum += nums[i];
+        }
+        if(sum%k != 0 || n < k) {
+            return false;
+        }
+        vector<int> dp(n, -1);
+        return kev(0 , k, 0, sum/k, nums, dp);
+        
+    }
+};
