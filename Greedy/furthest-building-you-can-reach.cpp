7f6342dc74382dfd5645990cf44290b3b064b406
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-19 23:37:43 +0600

    Update furthest-building-you-can-reach.cpp

--- Greedy/furthest-building-you-can-reach.cpp
+++ Greedy/furthest-building-you-can-reach.cpp
@@ -30,3 +30,34 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+/*
+understood this one , our basic idea is we keep on using bricks , now if we run out of bricks,
+we will take the largest difference, and then take back that difference and put it back in our brick value
+, why is that ? because the largest difference in the pq tells that we used that much brick, now lets take our brick back
+and use a ladder instead, we got no ladder ? that means we are stuck , so we will print the current index.
+*/
+public:
+    int furthestBuilding(vector<int>& h, int b, int lad) {
+        priority_queue<int> pq; 
+        for(int i = 0; i+1 < h.size();i ++){
+            int diff = h[i+1] - h[i];
+            if(diff > 0){
+                b -= diff;
+                pq.push(diff);
+                if(b < 0){
+                    if(lad <= 0){
+                        return i;
+                    }
+                    int take_back = pq.top();
+                    pq.pop();
+                    b += take_back;
+                    lad--;
+                }
+            }
+        }
+        return h.size()-1;
+    }
+};
