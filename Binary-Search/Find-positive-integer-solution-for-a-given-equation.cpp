41950790c6d3d031f03126aca7f56ea090c2a7c5
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-20 14:31:04 +0600

    Update Find-positive-integer-solution-for-a-given-equation.cpp

--- Binary-Search/Find-positive-integer-solution-for-a-given-equation.cpp
+++ Binary-Search/Find-positive-integer-solution-for-a-given-equation.cpp
@@ -27,3 +27,46 @@ public:
         return ans;
     }
 };
+
+
+
+/*
+ * // This is the custom function interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ * public:
+ *     // Returns f(x, y) for any given positive integers x and y.
+ *     // Note that f(x, y) is increasing with respect to both x and y.
+ *     // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ *     int f(int x, int y);
+ * };
+ */
+
+class Solution {
+//BS solution.
+public:
+    vector<vector<int>> findSolution(CustomFunction& cf, int z) {
+        vector<vector<int>> ans;
+        for(int i = 1; i <= 1000; i++){
+            int left = 1;
+            int right = 1000;
+            while(left <= right){
+                int mid = (left+right)/2;
+                if(cf.f(i,mid) == z){
+                    ans.push_back({i,mid});
+                    break;
+                }
+                else if(cf.f(i,mid) > z){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            }
+            if(cf.f(i,1) > z){
+                break;
+            }
+        }
+        return ans;
+    }
+};
