aac79009043ecc17ec7fcce3b1a13e7144475fa1
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-21 13:36:51 +0600

    Update Maximum-number-of-weeks-for-which-you-can-work.cpp

--- Greedy/Maximum-number-of-weeks-for-which-you-can-work.cpp
+++ Greedy/Maximum-number-of-weeks-for-which-you-can-work.cpp
@@ -22,3 +22,23 @@ public:
         return sum;
     }
 };
+
+
+class Solution {
+public:
+    long long numberOfWeeks(vector<int>& a) {
+        // I knew there was a trick to this, but i could not figure it out.
+        // The intuition is We take the max, if the max is greater than sum of rest(excluding the max element) of the elements.
+        // Then we use the formula rest * 2 + 1
+        // else we print the minimum of those those.
+
+        long long sum =0;
+        long long mx = 0;
+        for(int i = 0 ;i < a.size(); i++){
+            sum += a[i];
+            mx = max(mx, (long long)a[i]);
+        }
+        long long remaining_sum = sum - mx;
+        return min((remaining_sum * 2 + 1), sum);
+    }
+};
