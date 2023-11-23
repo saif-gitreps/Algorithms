50f5348e3c4dad54a57f7c457fcd63f598ff5b7e
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-23 23:52:52 +0600

    Update Longest-Happy-String.cpp

--- Greedy/Longest-Happy-String.cpp
+++ Greedy/Longest-Happy-String.cpp
@@ -37,3 +37,62 @@ public:
         return str;
     }
 };
+
+
+class Solution {
+// working solution.
+public:
+    string longestDiverseString(int a, int b, int c) {
+        //Correctly guessed we had to use Priority queue.
+        priority_queue<pair<int,char>> pq;
+        char ch = 'a';
+        if(a) pq.push({a,'a'});
+        if(b) pq.push({b,'b'});
+        if(c) pq.push({c,'c'});
+        int i = 0;
+        string str = "";
+        while(pq.size() > 1){
+            pair<int,char> one = pq.top();pq.pop();
+            pair<int,char> two = pq.top();pq.pop();
+            if(one.first >= 2){
+                str += one.second;
+                str += one.second;
+                one.first -= 2;
+            }
+            else{
+                str += one.second;
+                one.first -= 1;
+            }
+            // the and condition is to avoid a dual condition.
+            // if our second one is lesser than first one
+            // then at the next loop we can avoid aaa or bbb condition. We can implement the aabaabb sequence.
+            if(two.first >= 2 && two.first >= one.first){
+                str += two.second;
+                str += two.second;
+                two.first -= 2;
+            }
+            else{
+                str += two.second;
+                two.first -= 1; 
+            }
+            // update the value , put back in if theyre not 0.
+            if(two.first > 0){
+                pq.push(two);
+            }
+            if(one.first > 0){
+                pq.push(one);
+            }
+        }
+        if(pq.empty() == true){
+            return str;
+        }
+        if(pq.top().first>=2){
+            str += pq.top().second;
+            str += pq.top().second;
+        }
+        else{
+            str += pq.top().second;
+        }
+        return str;
+    }
+};
