32cc465eb7337b4956efe968c0039279a81ea68f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-24 00:05:17 +0600

    Update Longest-Happy-String.cpp

--- Greedy/Longest-Happy-String.cpp
+++ Greedy/Longest-Happy-String.cpp
@@ -96,3 +96,33 @@ public:
         return str;
     }
 };
+
+
+string longestDiverseString(int a, int b, int c) {
+    // better intuitive solution.
+        int total=a+b+c;
+        int currA=0, currB=0, currC=0;
+        string res = "";
+        while(total--){
+            
+            if((a>=b && a>=c && currA!=2) || (a>0 && (currB==2|| currC==2))){
+                res+='a'; 
+                a--;
+                currA++;
+                currB=currC=0;
+            }
+            else if((b>=a && b>=c && currB!=2) || (b>0 && (currA==2|| currC==2))){
+                res+='b';
+                b--;
+                currB++;
+                currA=currC=0;
+            }
+            else if((c>=a && c>=b && currC!=2) || (c>0 && (currA==2|| currB==2))){
+                res+='c';
+                c--;
+                currC++;
+                currA=currB=0;
+            }
+        }
+        return res;
+    }
