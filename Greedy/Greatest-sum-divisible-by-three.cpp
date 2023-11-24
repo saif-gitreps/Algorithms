31cc7098a10cce619ab59296408f365410562030
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-24 17:13:22 +0600

    Update Greatest-sum-divisible-by-three.cpp

--- Greedy/Greatest-sum-divisible-by-three.cpp
+++ Greedy/Greatest-sum-divisible-by-three.cpp
@@ -33,3 +33,39 @@ public:
         return kev(n-1, 0, nums, dp);
     }
 };
+
+
+class Solution {
+// This tabulation worked, but i got stuck in an edge case.
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        int sum = 0;
+        for(int i = 0 ; i < nums.size(); i++){
+            sum += nums[i];
+        }
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (sum+1, 0));
+        //return kev(n-1, 0, nums, dp);
+        for(int i = 0 ; i < n; i++){
+            for(int val = sum; val >= 1; val--){
+                if(i == 0){
+                    if(val % 3 == 0){
+                        dp[i][val] = val;
+                    }
+                    else{
+                        dp[i][val] = 0;
+                    }
+                    continue; 
+                }
+                int pick = 0;
+                int not_pick = 0;
+                if(val - nums[i] >= 0){
+                    pick = dp[i-1][val - nums[i]];
+                }
+                not_pick = dp[i-1][val];
+                dp[i][val] = max(pick, not_pick); 
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
