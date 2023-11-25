9511ce93e8ceee4bd777c3cb30dab42034b186e7
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-25 19:15:55 +0600

    Update Non-overlapping-Intervals.cpp

--- Greedy/Non-overlapping-Intervals.cpp
+++ Greedy/Non-overlapping-Intervals.cpp
@@ -25,6 +25,7 @@ public:
 //default was a[0] < b[0]. (why default? because it is giving the same result as before using a comparator).
 //apparently it was a[1] < b[1]
 // TC nlog n.
+//so after debuggin , the proper way of sorting was using the end interval whicch gave me the ideal sorted list.
 bool compare(vector<int> a, vector<int> b){
     return a[1] < b[1];
 }
@@ -48,3 +49,22 @@ public:
         return count;
     }
 };
+
+//This is not my code, but this is a good example of using a callback function inside as a comparator.
+int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.size() <= 1) return 0;     
+        
+		sort(intervals.begin(), intervals.end(), [](const auto& l, const auto& r) { 
+            return l[1] < r[1]; 
+        });     
+		
+        int prevIndex = 0;
+        int eraseCount = 0;
+        
+		for (int i = 1; i < intervals.size(); ++i) {
+			if (intervals[prevIndex][1] > intervals[i][0]) ++eraseCount;
+		    else prevIndex = i;
+		}
+        
+		return eraseCount;
+	}
