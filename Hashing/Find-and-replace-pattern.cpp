d5f967869b029b4d6bc667fbe67c6644674b88f2
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-11-28 22:46:33 +0600

    Update Find-and-replace-pattern.cpp

--- Hashing/Find-and-replace-pattern.cpp
+++ Hashing/Find-and-replace-pattern.cpp
@@ -33,3 +33,33 @@ public:
         return ans;
     }
 };
+
+
+
+
+string kev(string w) {
+        // its the same concept as my solution,
+        // We didnt need to declare another string var.
+        // We use the existign string Var, reducing SC.
+        unordered_map<char, int> mp;
+        for (char c : w){ 
+            if (!m.count(c)){ 
+                m[c] = m.size();
+            }
+        }
+        for (int i = 0; i < w.length(); ++i){
+            w[i] = 'a' + m[w[i]];
+        }
+        return w;
+    }
+    vector<string> findAndReplacePattern(vector<string> words, string p) {
+        // so this is basically a reduced version of my idea.
+        // Didnt get the idea of using a helper function.
+        // Helper function over here reduces the space complexity.
+            vector<string> ans;
+            string p = kev(p);
+            for (string w : words) if (kev(w) == p){
+                res.push_back(w);
+            }
+            return res;
+        }
