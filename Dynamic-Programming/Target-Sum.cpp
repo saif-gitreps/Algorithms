54b0ddef0d627c86ae5552d96da7bf1b8f45b712
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-02 14:43:25 +0600

    Update Target-Sum.cpp

--- Dynamic-Programming/Target-Sum.cpp
+++ Dynamic-Programming/Target-Sum.cpp
@@ -44,3 +44,55 @@ public:
         return kev(n-1, reqSubSum, nums, dp);
     }
 };
+
+
+class Solution {
+// tabulation.
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int sum = 0;
+        for(auto s: nums){
+            sum += s;
+        }
+        //eq1 : s1 + s2 = Ts -- (1)
+        //eq2 : s1 - s2 = target -- (2)
+        //solve for (3) =>
+        //s1(required subset sum) = (Ts - Target)/2
+        if(sum - target < 0){
+            return 0;
+        }
+        //difference between the total sum and target sum must be even.
+        if((sum - target) % 2 != 0){
+            return 0;
+        }
+        int reqSubSum = (sum - target)/2;
+        vector<vector<int>> dp(n, vector<int> (reqSubSum + 1, 0));
+        //return kev(n-1, reqSubSum, nums, dp);
+        for(int i = 0 ; i < n; i++){
+            for(int j = 0; j <= reqSubSum; j++){
+                if(i == 0){
+                    if(j == 0 && nums[i] == 0){
+                        dp[i][j] = 2;
+                    }
+                    else if(j == 0 || j == nums[i]){
+                        dp[i][j] = 1;
+                    }
+                    else{
+                        dp[i][j] = 0;
+                    }
+                }
+                else{
+                    int pick = 0;
+                    int not_pick = 0;
+                    if(j >= nums[i]){
+                        pick = dp[i-1][j - nums[i]];
+                    }
+                    not_pick = dp[i-1][j];
+                    dp[i][j] = pick + not_pick;
+                }
+            }
+        }
+        return dp[n-1][reqSubSum];
+    }
+};
