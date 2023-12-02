c12d360efefb8b1f6bd78c61597b5449f15881d7
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-03 01:11:56 +0600

    Update Top-K-Frequent-Element.cpp

--- Hashing/Top-K-Frequent-Element.cpp
+++ Hashing/Top-K-Frequent-Element.cpp
@@ -19,3 +19,26 @@ public:
         return ans;    
     }
 };
+
+class Solution {
+// i kind of cant find better solution than mine, but here is a code that appararently heapifies.
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+        for (int x : nums) cnt[x] += 1;
+
+        vector<pair<int, int>> maxHeap;
+        for (auto [num, freq] : cnt) maxHeap.emplace_back(freq, num);
+        make_heap(maxHeap.begin(), maxHeap.end()); // default is maxHeap
+
+        vector<int> ans;
+        while (k-- > 0) {
+            auto [_, num] = maxHeap[0];
+            pop_heap(maxHeap.begin(), maxHeap.end());
+            maxHeap.pop_back(); // delete the unused last element
+
+            ans.push_back(num);
+        }
+        return ans;
+    }
+};
