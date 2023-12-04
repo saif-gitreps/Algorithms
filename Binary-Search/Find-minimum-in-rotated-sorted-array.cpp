4bb32a3cbea2a901005c215f5935413194533dfd
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-05 00:17:00 +0600

    Update Find-minimum-in-rotated-sorted-array.cpp

--- Binary-Search/Find-minimum-in-rotated-sorted-array.cpp
+++ Binary-Search/Find-minimum-in-rotated-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 // i would say it was a good attempt.
+// update : adding a break, made it pass 87/150 cases.
 public:
     int findMin(vector<int>& nums) {
         int mn = nums[0];
@@ -13,6 +14,9 @@ public:
             }
             else if(mid-1 >=0 && mid+1 < nums.size() && nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1]){
                 return nums[mid+1];
+            }
+            else{
+                break;
             }
         }
         return mn;
