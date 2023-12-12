ec6879490b3a85dbac74b5498a3819ee39baa882
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-12 18:56:08 +0600

    Update Minimum-Time-to-Make-Rope-Colorful.cpp

--- Greedy/Minimum-Time-to-Make-Rope-Colorful.cpp
+++ Greedy/Minimum-Time-to-Make-Rope-Colorful.cpp
@@ -20,3 +20,21 @@ public:
         return count;  
     }
 };
+class Solution{
+// idk why i coudnt come with solutions like these, 
+// so here we just take the full sum till a unique balloon comes, at the same time we keep track of the max till that unique ballon.
+// Then we minus the max from the sum and just count += it.
+    int minCost(string s, vector<int>& cost) {
+        int res = 0, max_cost = 0, sum_cost = 0, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && s[i] != s[i - 1]) {
+                res += sum_cost - max_cost;
+                sum_cost = max_cost = 0;
+            }
+            sum_cost += cost[i];
+            max_cost = max(max_cost, cost[i]);
+        }
+        res += sum_cost - max_cost;
+        return res;
+    }
+};
