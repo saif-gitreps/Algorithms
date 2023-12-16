8eae38dbad2e8fc550501c9e8da9d4197cf140f1
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-16 13:40:11 +0600

    Update Rearrange-Words-In-A-Sentence.cpp

--- String/Rearrange-Words-In-A-Sentence.cpp
+++ String/Rearrange-Words-In-A-Sentence.cpp
@@ -37,3 +37,55 @@ public:
         return str;
     }
 };
+
+
+// This works but the edge case is dupilcate words.
+
+map<string,queue<int>> mp;
+bool compare(string a, string b){
+    if(a.size() == b.size()){
+        if(mp[a].front() < mp[b].front()){
+            mp[b].pop();
+            return true;
+        }    
+        else
+            return false;
+    }
+    if(a.size() < b.size()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+class Solution {
+public:
+    string arrangeWords(string text) {
+        vector<string> a;
+        string str = "";
+        text[0] = text[0] + 32;
+        int k = 0;
+        for(int i = 0 ; i < text.size(); i++){
+            if(text[i] == ' '){
+                a.push_back(str);
+                mp[str].push(k);
+                str = "";
+                k++;
+                continue;
+            }
+            str += text[i];
+        }
+        a.push_back(str);
+        mp[str].push(k);
+        sort(a.begin(), a.end(), compare);
+        str = "";
+        for(auto x : a){
+            str += x;
+            str += " ";
+        }
+        str.pop_back();
+        str[0] = str[0] - 32;
+        return str;
+    }
+};
