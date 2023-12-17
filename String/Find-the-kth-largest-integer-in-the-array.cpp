99ecf73d42e42b3f7d114eeb0ab768c3c45421f9
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2023-12-17 23:38:18 +0600

    Update Find-the-kth-largest-integer-in-the-array.cpp

--- String/Find-the-kth-largest-integer-in-the-array.cpp
+++ String/Find-the-kth-largest-integer-in-the-array.cpp
@@ -9,3 +9,25 @@ public:
         return nums[nums.size() - k];      
     }
 };
+
+
+
+bool compare(string a , string b){
+    if(a.size() < b.size()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+class Solution {
+// much better result, we first sort it in terms of order.
+// then we sort it in terms of size. 
+// worked till  180/210 cases.
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end(), compare);
+        return nums[nums.size() - k];      
+    }
+};
