4058a1e1c0ffbd296c0ec4a7031795d596fe4691
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-01 19:10:48 +0600

    Update Wiggle-Subsequence(dp).cpp

--- Dynamic-Programming/Wiggle-Subsequence(dp).cpp
+++ Dynamic-Programming/Wiggle-Subsequence(dp).cpp
@@ -40,3 +40,36 @@ public:
         return kev(1 ,prev, pos, nums, nums.size()) + 1;
     }
 };
+
+class Solution{
+// the perfect dp solution that didnt come to my mind.
+// Here in the dp , we are doing recurison on difference , and not numbers.
+// And i had doubts about the first initial value as well here after seeing this i am getting some ideas.
+public:
+    int solve(vector<int> &temp,vector<int> &dp,int ind, int prev,int n)
+    {
+        if(ind>=n)
+            return 0;
+        if(dp[prev+1]!=-1)
+            return dp[prev+1];
+        int take=0, nottake=solve(temp,dp,ind+1,prev,n);
+        if(prev==-1 || (temp[prev]<0 && temp[ind]>=0) || (temp[prev]>=0 && temp[ind]<=0))
+            take=1+solve(temp,dp,ind+1,ind,n);
+        return dp[prev+1]=max(take,nottake);
+    }
+    int wiggleMaxLength(vector<int>& nums) {
+        int n=nums.size();
+        if(n<2)
+            return n;
+        vector<int> temp;
+        for(int i=1;i<nums.size();i++)
+        {
+            int diff=nums[i]-nums[i-1];
+            if(diff!=0)
+                temp.push_back(diff);
+        }
+        vector<int> dp(n,-1);
+        int size=temp.size();
+        return 1+solve(temp,dp,0,-1,size);
+    }
+}
