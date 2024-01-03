252d86e667f88eae787515d096c58ff0ecd77959
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-03 20:14:34 +0600

    Update Longest-Palindromic-Subsequence.cpp

--- Dynamic-Programming/Longest-Palindromic-Subsequence.cpp
+++ Dynamic-Programming/Longest-Palindromic-Subsequence.cpp
@@ -67,3 +67,28 @@ public:
         return kev(n-1, n-1, s, s2, dp);
     }
 };
+
+
+class Solution {
+// similar solution to longest common subsequence.
+public:
+    int longestPalindromeSubseq(string text1) {
+        vector<vector<int>> dp(text1.size()+1, vector<int> (text1.size()+1, 0));
+        string text2 = text1;
+        reverse(text1.begin(), text1.end());
+        for(int i = 0 ; i <= text1.size() ; i ++){
+            for(int j = 0 ; j <= text2.size() ; j ++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if(text1[i - 1] == text2[j - 1]){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } 
+                else{
+                    dp[i][j] = max(dp[i - 1][j] , dp[i][j -  1]);
+                }
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
