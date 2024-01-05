83677fba4b1e5bcca817225715917045688cfc0c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-05 19:35:31 +0600

    Update Longest-Palindromic-Substring.cpp

--- Dynamic-Programming/Longest-Palindromic-Substring.cpp
+++ Dynamic-Programming/Longest-Palindromic-Substring.cpp
@@ -48,3 +48,90 @@ public:
         return ans;
     }
 };
+
+class Solution {
+/*
+Erichto's solution, Ok so i understood this , we bascially keep a pointer at an index,
+and then we expand from that point, while expanding we check if left and right chars are equal
+EVery iteration of mid, we pick N points (entire string) then we expand.
+The second part is for even characters.
+When x = 1 -> mid, mid + 1 -> AA
+                              ^^
+when x = 2 -> mid - 1, mid + 2 ->  AAAA
+                                   ^  ^
+
+*/
+public: 
+    string longestPalindrome(string s) {
+        int best_len = 0;
+        string best_s = "";
+        int n = s.length();
+        for(int mid = 0; mid < n; mid++) {
+            for(int x = 0; mid - x >= 0 && mid + x < n; x++) {
+                if(s[mid-x] != s[mid+x]) {
+                    break;
+                }
+                int len = 2 * x + 1;
+                if(len > best_len) {
+                    best_len = len;
+                    best_s = s.substr(mid - x, len);
+                }
+            }
+        }
+        for(int mid = 0; mid < n - 1; mid++) {
+            // so n - 1 because for our mid point issue, 
+            // lets say "abba".
+            // our mid point here is the first 'b' and expansion starts from there.
+            for(int x = 1; mid - x + 1 >= 0 && mid + x < n; x++) {
+                if(s[mid-x+1] != s[mid+x]) {
+                    break;
+                }
+                int len = 2 * x;
+                if(len > best_len) {
+                    best_len = len;
+                    best_s = s.substr(mid - x + 1, len);
+                }
+            }
+        }
+        return best_s;
+    }
+};
+
+class Solution{
+ // O(n^3) solution. slightly optimized for the inner loop.
+ /*
+   Here, we first start by marking an imaginary dp table, 
+   we first mark all the 1 length chars as 1, then we mark 0-2, 1-3, 2-4
+   then 0-3, 1-4, 2-5 so on , then we check if the inner of them was palindrome.
+
+   But this solution was different from the explanation:
+   https://www.youtube.com/watch?v=UflHuQj6MVA
+ */
+
+public:
+  int longestPalSubstr(string str){
+    
+      int n = str.size();
+      int maxLength = 1, start = 0;
+   
+      for (int i = 0; i < str.length(); i++) {
+          for (int j = i; j < str.length(); j++) {
+              int flag = 1;
+              for (int k = 0; k < (j - i + 1) / 2; k++)
+                  if (str[i + k] != str[j - k])
+                      flag = 0;
+   
+              if (flag && (j - i + 1) > maxLength) {
+                  start = i;
+                  maxLength = j - i + 1;
+              }
+          }
+      }
+   
+      cout << "Longest palindrome substring is: ";
+      printSubStr(str, start, start + maxLength - 1);
+      return maxLength;
+  }
+};
+
+// Two more algorithms to review.
