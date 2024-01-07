af46f6cfb9a414680ba98debfb94f7c344b33ed7
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-08 01:21:05 +0600

    Update Minimum-Genetic-Mutation.cpp

--- BFS-DFS/Minimum-Genetic-Mutation.cpp
+++ BFS-DFS/Minimum-Genetic-Mutation.cpp
@@ -30,3 +30,44 @@ public:
         return -1;      
     }
 };
+
+class Solution {
+// working solution , we bascially bruteforce thorugh each char and check if it wasnt visited and if it is in bank then we do a bfs.
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> vis;
+        queue<string> q;
+        q.push(startGene);
+        vis.insert(startGene);
+        int count = 0;
+
+        while(q.empty() == false){
+            int n = q.size();
+            
+            for(int i = 0; i < n; i++){
+                string curr_node = q.front();
+                q.pop();
+
+                if(curr_node == endGene){
+                    return count;
+                }
+
+                for(auto c: "ACGT"){
+                    
+                    for(int j = 0; j < curr_node.size(); j++){
+                        string check_node = curr_node;
+                        check_node[j] = c;
+                        if(!vis.count(check_node) && 
+                            find(bank.begin(), bank.end() ,check_node) != bank.end()){
+                                
+                            q.push(check_node);
+                            vis.insert(check_node);
+                        }
+                    }
+                }
+            }
+            count++;
+        }
+        return -1;
+    }
+};
