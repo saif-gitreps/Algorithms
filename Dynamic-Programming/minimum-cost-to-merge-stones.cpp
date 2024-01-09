33500651f2b54afb55bc2f8dc80ff3bfc828a8e2
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-09 16:01:11 +0600

    Update minimum-cost-to-merge-stones.cpp

--- Dynamic-Programming/minimum-cost-to-merge-stones.cpp
+++ Dynamic-Programming/minimum-cost-to-merge-stones.cpp
@@ -1,7 +1,10 @@
 class Solution {
-// getting on the right track here, have to figure out -1 cases.
+// tis recursion worked, the -1 case could be solved with a check.
 public:
     int kev(int i, vector<int> a, int k){
+        if(a.size() < k){
+            return -1;
+        }
         if(a.size() == k){
             int sum = 0;
             for(auto nums : a){
@@ -9,15 +12,17 @@ public:
             }
             return sum;
         }
-        if(a.size() < k){
-            return -1;
-        }
         int pick = INT_MAX;
         int not_pick = INT_MAX;
         int sum = 0;
         vector<int> temp;
+        int flag = true;
         for(int ind = 0; ind < a.size(); ind ++){
             if(ind == i){
+                if(k > a.size()){
+                    flag = false;
+                    break;
+                }
                 for(int p = k;ind  < a.size() && p >= 1; p-- ){
                     sum += a[ind++];
                 }
@@ -28,13 +33,21 @@ public:
                 temp.push_back(a[ind]);
             }
         }
-        pick = sum + kev(0, temp, k);
+        if(flag){
+            pick = sum + kev(0, temp, k);
+        }
         if(i + k < a.size()){
             not_pick = kev(i + 1, a, k);
         }
         return min(pick, not_pick);
     }
     int mergeStones(vector<int>& stones, int k) {
+        int n = stones.size();
+        if ((n - 1) % (k - 1)) 
+            return -1;
+        if(stones.size() == 1){
+            return 0;
+        }
         return kev(0, stones, k);
     }
 };
