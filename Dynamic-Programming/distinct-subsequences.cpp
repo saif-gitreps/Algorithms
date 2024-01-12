c820f6e1066d5fd8e5a42a72df0a2791c1bf2661
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-12 15:42:30 +0600

    Update distinct-subsequences.cpp

--- Dynamic-Programming/distinct-subsequences.cpp
+++ Dynamic-Programming/distinct-subsequences.cpp
@@ -31,5 +31,40 @@ public:
         vector<int> dp(n + 1, -1);
         return kev(0 ,"", s, t, dp);
     }
+};
+
+class Solution {
+// i had to think differently.
+public:
+    int kev(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(j == t.size()){
+            // so when j gets full that means we found a subsequence.
+            return 1;  
+        }
+        if(i == s.size()){
+            // when i becomes full that means we ran out of S .
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int pick = 0;
+        int not_pick = 0;
+        if(s[i] == t[j]){
+            // so here if we match a char, we can make two number of moves, 
+            // we move both index together, or we move the s index and keep t index constant to see for more subsequence.
+            pick = kev(i+1, j+1, s, t, dp) + kev(i+1, j, s , t, dp);
+        }
+        else{
+            not_pick = kev(i+1, j, s, t, dp);
+        }
+        return dp[i][j] = pick + not_pick;
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return kev(0 , 0, s, t, dp);
+    }
 
 };
