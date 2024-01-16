c829490905ecfcef6393a3444413a6267766f62f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-16 15:41:01 +0600

    Update Edit-Distance.cpp

--- Dynamic-Programming/Edit-Distance.cpp
+++ Dynamic-Programming/Edit-Distance.cpp
@@ -44,3 +44,30 @@ public:
         return kev(n - 1, m - 1, s, t, dp);
     }
 };
+
+class Solution {
+// did tab successfully.
+public:
+    int minDistance(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        for(int j = 0 ; j <= m; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = i;
+        }
+        for(int i = 1; i <= n ; i ++){
+            for(int j = 1; j <= m; j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
