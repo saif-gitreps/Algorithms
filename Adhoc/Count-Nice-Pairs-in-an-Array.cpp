676c3d2e78dd70be0f46ee94e9287acfaab18456
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-20 21:17:59 +0600

    Update Count-Nice-Pairs-in-an-Array.cpp

--- Adhoc/Count-Nice-Pairs-in-an-Array.cpp
+++ Adhoc/Count-Nice-Pairs-in-an-Array.cpp
@@ -19,3 +19,25 @@ public:
         return ans;
     }
 };
+
+class Solution {
+// forgot to keep mod in acccount also hint 3 helped.
+public:
+    int countNicePairs(vector<int>& nums) {
+        unordered_map<long long, long long> mp;
+        int ans = 0;
+        for(int i = 0 ; i < nums.size(); i++){
+            int rev = 0;
+            int temp = nums[i];
+            while(temp > 0){
+                rev = rev * 10 + (temp % 10);
+                temp /= 10;
+            }
+            ans = (ans + mp[nums[i] - rev] ++) % int(1e9 + 7);
+        }
+        // for(auto s : mp){
+        //     ans += (s.second * ( s.second - 1) / 2);
+        // }
+        return ans;
+    }
+};
