7ead07d0ca718841b59ff528d0b5dd620ac9b819
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-21 21:59:08 +0600

    Update Find-Minimum-In-Rotated-Sorted-Array.cpp

--- Binary-Search/Find-Minimum-In-Rotated-Sorted-Array.cpp
+++ Binary-Search/Find-Minimum-In-Rotated-Sorted-Array.cpp
@@ -29,3 +29,32 @@ public:
         return mn;
     }
 };
+
+
+class Solution {
+// YAAAAAAASSSSSSSSSSSSS , solved it on my own yay
+// intuition was, if we encounter similar numbers on either left or right
+// Then we trim both sides. and continue our BS search.
+// TC -> O(n) for Worst case. 
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size()-1;
+        int mn = INT_MAX; 
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            mn = min(mn, nums[mid]);
+            if(nums[left] == nums[right]){
+                mn = min(nums[left], mn);
+                left++; right--;
+            }            
+            else if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return mn;
+    }
+};
