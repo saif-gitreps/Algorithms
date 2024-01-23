7ae2a06d47b7ba440701123c9ef9ad3c33b27b7c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-23 12:47:06 +0600

    Update Wildcard-Matching.cpp

--- Dynamic-Programming/Wildcard-Matching.cpp
+++ Dynamic-Programming/Wildcard-Matching.cpp
@@ -40,3 +40,48 @@ public:
         return kev(s.size()-1, p.size()-1, s, p, '-1');
     }   
 };
+
+
+class Solution {
+// This worked but shows MLE because of the stack space usage,
+// So the doubt I had with * was, can it not go infinite when it is matching with space, 
+// well it will only be accepted for empty strings if the rest of the P is *, else it won't.
+// also we are brute-forcing the * case, one Kev will ignore the star, and the other will match the start and move forward.
+// now in this case we won't be matching with the empty strings because The remaing of the P is not * only.
+// The problem statement was vague AF, I didn't understand the problem properly.
+public:
+    int kev(int i, int j,string s, string p, vector<vector<int>> &dp){
+        if(i < 0 && j < 0){
+            return true;
+        }
+        if(j < 0 && i >= 0){
+            return false;
+        }
+        if(i < 0 && j >= 0){
+            for(int remaining = 0; remaining <= j; remaining++){
+                if(p[remaining] != '*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int ans = 0;
+        if(s[i] == p[j] || p[j] == '?'){
+            ans += kev(i - 1, j - 1, s, p, dp);
+        }
+        if(p[j] == '*'){
+            // we either ignore the start and move forward, or we match the i char of S and continue.
+            ans += kev(i - 1, j , s ,p ,dp) || kev(i , j - 1, s, p, dp);
+        }
+        return dp[i][j] = ans;
+    }
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return kev(n - 1, m - 1, s,  p, dp);
+    }  
+};
