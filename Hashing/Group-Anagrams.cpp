5ce280868e7371eb3f4b5fdc05b456638224d738
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-24 15:21:58 +0600

    Update Group-Anagrams.cpp

--- Hashing/Group-Anagrams.cpp
+++ Hashing/Group-Anagrams.cpp
@@ -17,3 +17,23 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+// i am so stupid, i could have just sorted it out instead of using a whole ass Map to store it.
+// anyway atleast the rest of the solution and intuition was good.
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>> mp;
+        for(auto str : strs){
+            string temp = str;
+            sort(temp.begin(), temp.end());
+            mp[temp].push_back(str);
+        }
+        vector<vector<string>> ans;
+        for(auto arr : mp){
+            ans.push_back(arr.second);
+        }
+        return ans;
+    }
+};
