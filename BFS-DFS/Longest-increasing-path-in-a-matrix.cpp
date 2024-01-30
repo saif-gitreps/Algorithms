262f2e9a053de6014f0054c52171349495b4d0fa
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-30 18:39:21 +0600

    Update Longest-increasing-path-in-a-matrix.cpp

--- BFS-DFS/Longest-increasing-path-in-a-matrix.cpp
+++ BFS-DFS/Longest-increasing-path-in-a-matrix.cpp
@@ -41,3 +41,40 @@ public:
         return mx;
     }
 };
+
+
+class Solution {
+// had similar idead but the missing implementation was the prev one , how would i avoid conflict of the first 
+// starting point because of <= condition. Like we have to ignore if the element is less than or equal to.
+// we can avoid this first element issue by assigning it as -1.
+// also we are memoizing matrix index of each element.
+public:
+    int dfs(int i, int j, vector<vector<int>> &matrix, int prev, map<pair<int,int> , int> &dp){
+        if(i < 0 || j < 0 || i >= matrix.size() || 
+        j >= matrix[0].size() ||
+        matrix[i][j] <= prev){
+            return 0;
+        }
+        if(dp.find({i, j}) != dp.end()){
+            return dp[{i, j}];
+        }
+        int ans = 1;
+        ans = max(ans, 1 + dfs(i + 1, j, matrix, matrix[i][j], dp));
+        ans = max(ans, 1 + dfs(i - 1, j, matrix, matrix[i][j], dp));
+        ans = max(ans, 1 + dfs(i, j + 1, matrix, matrix[i][j], dp));
+        ans = max(ans, 1 + dfs(i, j - 1, matrix, matrix[i][j], dp));
+        return dp[{i, j}] = ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int mx = 1;
+        map<pair<int, int>, int> dp;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                mx = max(mx, dfs(i, j, matrix, -1, dp));
+            }
+        }
+        return mx;
+    }
+};
