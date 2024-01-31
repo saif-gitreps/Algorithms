752a47f88a6f33f557357c6634dbf827b4f52a2f
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-01-31 20:18:22 +0600

    Update Best-time-to-buy-and-sell-stock-ii.cpp

--- Dynamic-Programming/Best-time-to-buy-and-sell-stock-ii.cpp
+++ Dynamic-Programming/Best-time-to-buy-and-sell-stock-ii.cpp
@@ -33,3 +33,36 @@ public:
 
     }
 };
+
+
+class Solution {
+// had confusion about the buy as well, just make a loop for all the changing parameters
+// and copy the recurrence.
+public:
+    int n;
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        vector<vector<int>> dp(n+1 , vector<int> (2, -1));
+        dp[n][1] = 0;
+        dp[n][0] = 0;
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = 0; j <= 1; j++){
+                int profit = 0;
+                if(j){
+                    profit = max(
+                        (-1*prices[i]) + dp[i + 1][0],
+                        dp[i + 1][1]
+                    );
+                }
+                else{
+                    profit = max(
+                        prices[i] + dp[i + 1][1],
+                        dp[i + 1][0]
+                    );
+                }
+                dp[i][j] = profit;
+            }
+        } 
+        return dp[0][1];
+    }
+};
