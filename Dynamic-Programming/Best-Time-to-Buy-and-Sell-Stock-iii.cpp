68f47d8b6330da1b6846e2e6bb31bd8429dd067e
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-02-08 21:48:26 +0600

    Update and rename best-time-to-buy-and-sell-stock-iii.cpp to Best-Time-to-Buy-and-Sell-Stock-iii.cpp

--- Dynamic-Programming/Best-Time-to-Buy-and-Sell-Stock-iii.cpp
+++ Dynamic-Programming/Best-Time-to-Buy-and-Sell-Stock-iii.cpp
@@ -36,3 +36,34 @@ public:
 
     }
 };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+                    if (buy == 0) { 
+                        dp[ind][buy][cap] = max(
+                            0 + dp[ind + 1][0][cap],
+                            -prices[ind] + dp[ind + 1][1][cap]
+                        );
+                    }
+
+                    if (buy == 1) {
+                        dp[ind][buy][cap] = max(
+                            0 + dp[ind + 1][1][cap],
+                            prices[ind] + dp[ind + 1][0][cap - 1]
+                        );
+                    }
+                }
+            }
+        }
+
+    return dp[0][0][2];
+
+    }
+};
