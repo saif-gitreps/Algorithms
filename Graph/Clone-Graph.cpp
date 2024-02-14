5035d17c6a3d390bd0030b774cd6781172daa143
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-02-14 15:07:20 +0600

    Update Clone-Graph.cpp

--- Graph/Clone-Graph.cpp
+++ Graph/Clone-Graph.cpp
@@ -47,3 +47,40 @@ public:
         return dfs(node, vis, copies);
     }
 };
+
+
+
+/*
+    a Bit more optimized , do a dry run on paper, you ll understand.
+    dry run this one, [2,4] , [1, 3], [2, 4] , [1, 3].
+    1 --------- 2
+    |           |
+    |           |
+    |           |
+    4-----------3
+*/
+
+class Solution {
+public:
+    Node* dfs(Node *source, map<Node*, Node*> &copies){
+        if (source == nullptr)
+            return nullptr;
+
+        Node* sourceNode = new Node(source->val);
+        copies[source] = sourceNode;
+        for(auto node: source->neighbors){
+            if (copies.find(node) == copies.end()) {
+                sourceNode->neighbors.push_back(dfs(node, copies));
+            }
+            else {
+                sourceNode->neighbors.push_back(copies[node]);
+            }
+        }
+    return sourceNode;
+}
+
+    Node* cloneGraph(Node* node) {
+        map<Node*, Node*> copies;
+        return dfs(node, copies);
+    }
+};
