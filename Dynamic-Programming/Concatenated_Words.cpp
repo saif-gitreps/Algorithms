4b6f852e438404500d087048c7d4e927d0570dd3
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-03-14 17:37:16 +0600

    Update Concatenated_Words.cpp

--- Dynamic-Programming/Concatenated_Words.cpp
+++ Dynamic-Programming/Concatenated_Words.cpp
@@ -46,3 +46,35 @@ public:
         return ans;
     }
 };
+
+class Solution {
+// take a mix word, and do a two pointer and take differencet substring, and mark it 1 if found.
+// then to put in inside answer , check the last index, if the last index is 1, then that means an entire substring can be formed
+
+
+public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        unordered_set<string> words_set;
+        for (string word : words) words_set.insert(word);
+        vector<string> res;
+    
+        for (string word : words) {
+            int n = word.size();
+            vector<int> dp(n + 1, 0);
+            dp[0] = 1;
+            for (int i = 0; i < n; i++) {
+                if (!dp[i]) continue;
+                for (int j = i + 1; j <= n; j++) {
+                    if (j - i < n && words_set.count(word.substr(i, j - i))) {
+                        dp[j] = 1;
+                    }
+                }
+                if (dp[n]) {
+                    res.push_back(word);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
