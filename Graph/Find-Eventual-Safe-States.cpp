e3cd8f3200b57e585d1284820cf125eb7c20c883
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-03-19 01:32:55 +0600

    Update Find-Eventual-Safe-States.cpp

--- Graph/Find-Eventual-Safe-States.cpp
+++ Graph/Find-Eventual-Safe-States.cpp
@@ -43,3 +43,37 @@ public:
         return ans;
     }
 };
+
+class Solution {
+// genious solution: link to explanation: https://leetcode.com/problems/find-eventual-safe-states/solutions/2600013/c-topological-sort-diagram/
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> inComingNodes(n);
+        vector<int> outDeg(n), ans;
+        queue<int> q;
+
+        for(int i = 0; i < n; i++){
+            for(auto child: graph[i]){
+                inComingNodes[child].push_back(i);
+            }
+            outDeg[i] = graph[i].size();
+            if(graph[i].size() == 0){
+                q.push(i);
+            }
+        }
+
+        while(q.size()){
+            int node = q.front(); q.pop();
+            ans.push_back(node);
+
+            for(auto child: inComingNodes[node]){
+                if(--outDeg[child] == 0){
+                    q.push(child);
+                }
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
