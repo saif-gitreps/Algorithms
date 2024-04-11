430e9f5ea9da1549e937829eae0178f481650630
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-04-11 22:28:16 +0600

    Update Cookies.cpp

--- Adhoc/Cookies.cpp
+++ Adhoc/Cookies.cpp
@@ -1,42 +1,24 @@
-question in a real interview before?  
-Subscribe to see which companies asked this question.
-
-Related Topics 
-
-C++	
-1
 class Solution {
-2
 public:
-3
     int findContentChildren(vector<int>& g, vector<int>& s) {
-4
-        int mx = 0;
-5
-        for(auto n : s){
-6
-            mx = max(mx, n);
-7
-        }
-8
-​
-9
+        int i = 0;
+        int j = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin() , s.end());
         int count = 0;
-10
-        for(auto n: g){
-11
-            if(n >= mx){
-12
-                count++;
-13
+        while (i < g.size() && j < s.size()){
+            if(g[i] <= s[j]){
+                i++;j++; count++;
+                }
+            else{
+                j++;
+                
             }
-14
-        }
-15
-        
-16
+                
+                
+
+            }
         return count;
-17
-    }
-18
+        }
 };
+
