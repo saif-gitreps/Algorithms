3668fe8e96df2ad050f6819af035175c3d35e67e
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-04-13 23:58:36 +0600

    Update Search-Suggestions.cpp

--- Binary-Search/Search-Suggestions.cpp
+++ Binary-Search/Search-Suggestions.cpp
@@ -16,7 +16,10 @@ public:
             
             for (int i = 0; i < 3 and it + i != p.end(); i++){
                 string& temp = *(it + i);
- //If temp doesn't start with str, then break               
+ /* If temp doesn't start with str, then break, So understood this ,
+Basically find will return the index of the occurense of substring in A.find(B), 
+if B is a substr found at index 0 or starting then we know we can take it. 
+If the find returns any value from 1 till end that means the found product does not have a prefix with the search word */            
 if (temp.find(str)) 
                     break;
                 w.push_back(temp);
