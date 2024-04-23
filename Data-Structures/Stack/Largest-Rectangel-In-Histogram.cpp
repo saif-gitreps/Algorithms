c5fe3c941ac35a225fb450aa53c2a593f21268c6
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-04-23 20:12:24 +0600

    Update Largest-Rectangel-In-Histogram.cpp

--- Data-Structures/Stack/Largest-Rectangel-In-Histogram.cpp
+++ Data-Structures/Stack/Largest-Rectangel-In-Histogram.cpp
@@ -20,3 +20,49 @@ public:
         return mx;
     }
 };
+
+// better solution, O(n) technically O(3n).
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& h) {
+        int n = h.size();
+        stack<int> st;
+        vector<int> left(n, 0), right(n, 0);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && h[st.top()] >= h[i]){
+                st.pop();
+            }
+
+            if(st.empty())
+                left[i] = 0;
+            else
+                left[i] = st.top() + 1;
+            
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            st.pop();
+        }
+
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && h[st.top()] >= h[i]){
+                st.pop();
+            }
+
+            if(st.empty())
+                right[i] = n - 1;
+            else
+                right[i] = st.top() - 1;
+            
+            st.push(i);
+        }
+
+        int mx = 0;
+        for(int i = 0; i < n; i++){
+            int height = h[i] * (right[i] - left[i] + 1);
+            mx = max(mx, height);
+        }
+        return mx;
+    }
+};
