83cdf9806e8ac70fe76183dcb0c2c764984c4f69
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-04-30 23:22:22 +0600

    Update Minimum-Ascii-Delete-Sum-for-Two-Strings.cpp

--- Dynamic-Programming/Minimum-Ascii-Delete-Sum-for-Two-Strings.cpp
+++ Dynamic-Programming/Minimum-Ascii-Delete-Sum-for-Two-Strings.cpp
@@ -1,5 +1,5 @@
 class Solution {
-// intuition was, it is the same as Longest common subsequence but instead we are dealing with ASCIS.
+// intuition was, that it is the same as the Longest common subsequence but instead, we are dealing with ASCIS.
 // Here this sol showed MLE.
 public:
     vector<vector<int>> dp;
@@ -38,5 +38,49 @@ public:
         }
 
         return (total1 - res) + (total2 - res);
+    }
+};
+
+
+class Solution {
+// Successfully turned the solution into top-down tabulation, which worked.
+public:
+    vector<vector<int>> dp;
+    
+    int minimumDeleteSum(string s1, string s2) {
+        int n = s1.size(); int m = s2.size();
+
+        dp.resize(n + 1, vector<int> (m + 1, -1));
+        
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(s1[i - 1] == s2[j - 1]){
+                    dp[i][j] = s1[i - 1] + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = max(
+                        dp[i - 1][j],
+                        dp[i][j - 1]
+                    );
+                }
+            }
+        }
+
+        int total1 = 0, total2 = 0;
+        for(auto s : s1){
+            total1 += s;
+        }
+        for(auto s : s2){
+            total2 += s;
+        }
+
+        return (total1 - dp[n][m]) + (total2 - dp[n][m]);
     }
 };
