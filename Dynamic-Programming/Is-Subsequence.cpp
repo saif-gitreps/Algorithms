e6563281805a1c4c2a65c54ebf226d20b7613dfb
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-05-16 00:52:43 +0600

    Update Is-Subsequence.cpp

--- Dynamic-Programming/Is-Subsequence.cpp
+++ Dynamic-Programming/Is-Subsequence.cpp
@@ -37,3 +37,34 @@ public:
     }
 };
 
+class Solution {
+// Idea is we keep a map of all chars of T with an array of indexes on which those characters were encountered.
+// Then we traverse every char of S. We check if the current Char doesnt exist then return false.
+// Then we take the index array. We try to find the element based on the fact that
+// lowerbound is used to keep the relative order of the subsequence.
+// s = ace is subseq of t = abcde because a then c then e is in the relative order inside t.
+// This helps us avoid the cases where a char may exist but it way behind , and it is not suitable character 
+// because it does not maintain the relative ordering.
+public:
+    bool isSubsequence(string s, string t) {
+        unordered_map<char, vector<int>> mp;
+        for(int i = 0; i < t.size();i ++){
+            mp[t[i]].push_back(i);
+        }
+
+        int relative_order = 0;
+        for(auto c : s) {
+            if(mp.find(c) == mp.end()){
+                return false;
+            }
+            vector<int> index_array = mp[c];
+            auto it = lower_bound(index_array.begin(), index_array.end(), relative_order);
+            if(it == index_array.end()) {
+                return false;
+            }
+            relative_order = *it + 1;
+        }
+        return true;
+    }
+};
+
