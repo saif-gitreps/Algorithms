c0eac9c4e7acb01db6fb90e9f42c00211b542dcb
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-05-18 20:36:09 +0600

    Create Minimum-Cost-For-Tickects.cpp

---
+++ Dynamic-Programming/Minimum-Cost-For-Tickects.cpp
@@ -0,0 +1,58 @@
+class Solution {
+// So this is a tough problem it seems. But thank god i at least figured out the recursion
+// I don't know why this algorithm passes 70/70 cases yet somehow it shows MLE.
+public:
+    vector<vector<int>> dp;
+    int kev(int i, int pass, vector<int> a, vector<int> c){
+        if(i == a.size()){
+            return 0;
+        }
+        if(dp[i][pass] != -1){
+            return dp[i][pass];
+        }
+        if(pass >= a[i]) {
+            return dp[i][pass] = kev(i + 1, pass, a, c);
+        }
+        return dp[i][pass] = min(
+            min(
+                c[0] + kev(i + 1, pass, a ,c),
+                c[1] + kev(i + 1, a[i] + 6, a, c)
+            ),
+            c[2] + kev(i + 1, a[i] + 29, a, c)
+        );
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        dp.resize(days.size(), (vector<int> (400, -1)));
+
+        return kev(0, 0, days, costs);
+    }
+};
+
+class Solution {
+// Used a map instead of 2Dp vector and it worked.
+public:
+    vector<vector<int>> dp;
+    int kev(int i, int pass, vector<int> &a, vector<int> &c, map<pair<int, int>, int> &dp){
+        if(i == a.size()){
+            return 0;
+        }
+        if(dp.find({i, pass}) != dp.end()){
+            return dp[{i, pass}];
+        }
+        if(pass >= a[i]) {
+            return dp[{i, pass}] = kev(i + 1, pass, a, c, dp);
+        }
+        return dp[{i, pass}] = min(
+            min(
+                c[0] + kev(i + 1, pass, a ,c, dp),
+                c[1] + kev(i + 1, a[i] + 6, a, c, dp)
+            ),
+            c[2] + kev(i + 1, a[i] + 29, a, c, dp)
+        );
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        map<pair<int, int>, int> mp;
+
+        return kev(0, 0, days, costs, mp);
+    }
+};
