40e889c90221fb3944cac22aed0a5d4efc094af8
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-05-20 14:32:28 +0600

    Update Minimum-Cost-For-Tickects.cpp

--- Dynamic-Programming/Minimum-Cost-For-Tickects.cpp
+++ Dynamic-Programming/Minimum-Cost-For-Tickects.cpp
@@ -56,3 +56,38 @@ public:
         return kev(0, 0, days, costs, mp);
     }
 };
+
+
+class Solution {
+// A better solution using just O(n) space, now this one solved my confusion of the Pass variable. How do we pass all the days that was to be skipped
+// because we picked a ticket(7 passes for +6 ticket). I didnt know that could have been solved with just a single loop till that index
+// instead of keeping a pass variable and "if(pass >= a[i]) return dp[{i, pass}] = kev(i + 1, pass, a, c, dp); ".        
+public:
+    int kev(int i,  vector<int> &a, vector<int> &c, vector<int> &dp){
+        if(i == a.size()){
+            return 0;
+        }
+        if(dp[i] != -1){
+            return dp[i];
+        }
+        
+        int seven_days = INT_MAX, thirty_days = INT_MAX, j = 0;
+        
+        for(j = i; j < a.size() && a[j] < a[i] + 7; j++);
+        seven_days = min(seven_days, c[1] + kev(j, a, c, dp));
+        
+        for(j = i; j < a.size() && a[j] < a[i] + 30; j++);
+        thirty_days = min(thirty_days, c[2] + kev(j , a, c, dp));
+
+        return dp[i] = min({
+            c[0] + kev(i + 1, a, c, dp),
+            seven_days,
+            thirty_days
+        });
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(days.size() + 1, -1);
+
+        return kev(0, days, costs, dp);
+    }
+};
