ae3ada9989a7f8b131f79a68c64aeaf49eee039d
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-06-16 18:33:53 +0600

    Update Pacific-Atlantic-Water-Flow.cpp

--- BFS-DFS/Pacific-Atlantic-Water-Flow.cpp
+++ BFS-DFS/Pacific-Atlantic-Water-Flow.cpp
@@ -90,3 +90,61 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+/*
+    Cells on the border (first/last row, and first/last column) are guaranteed to get into the ocean. 
+    You start off with the border cells and go from there to explore the inner land.
+
+    The point here, I think, is that we do not need to scan the entire matrix to find the "start point" of DFS, 
+    we just need to look at the first and last rows and columns. 
+    Because essentially we can find all points from where water can get to the ocean
+    by starting DFS at the border, and accepting the common point as answer.
+*/
+public:
+    vector<int> offset = {0, 1, 0, -1, 0};
+
+    bool outOfBounds(int row, int col, int m, int n){
+        return row < 0 || col < 0 || row > m -1 || col > n -1;
+    }
+
+    void dfs(vector<vector<int>>& matrix, vector<vector<int>>& visited, int i, int j) {
+        // m is for row and n is for col.
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        visited[i][j] = true;
+        for (int k =1 ; k<offset.size(); k++){
+            int row = i + offset[k], col = j + offset[k-1] ;
+            if( !outOfBounds(row, col, m, n) && !visited[row][col] && matrix[row][col] >= matrix[i][j])
+                    dfs(matrix, visited, row, col);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<int>> ans;
+        vector<vector<int>> pacific(n, vector<int> (m, 0));
+        vector<vector<int>> atlantic(n, vector<int> (m, 0));
+        // here n is row and m is for col.
+        for(int i = 0; i < n; i++ ){
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, m - 1);
+        }
+
+        for(int j = 0; j < m; j++){
+            dfs(heights, pacific, 0, j);
+            dfs(heights, atlantic, n - 1, j);
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(atlantic[i][j] && pacific[i][j])
+                    ans.push_back({i, j});
+            }
+        }
+
+        return ans;
+    }
+};
