ff394b88a7119dc4805b699d8f82e7c83c59678c
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-06-20 12:48:16 +0600

    Create Longest-String-Chain.cpp

---
+++ Dynamic-Programming/Longest-String-Chain.cpp
@@ -0,0 +1,51 @@
+class Solution {
+// I feel like the solution would have been much easier to solve if i sat down with a calm thought.
+// more like a DFS + memo
+public:
+    bool are_words_pre(string a, string b){
+        int count = 0;
+        
+        for(int i = 0, j = 0; i < a.size() && j < b.size(); j++){
+            if(a[i] != b[j])
+                count ++;
+            else
+                i++;
+        }
+
+        return count <= 1;
+    }
+    int kev(string word, unordered_map<string, int> &dp, map<int, vector<string>> &mp){
+        if(mp.find(word.size() + 1) == mp.end())
+            return 1;
+        
+        if(dp.find(word) != dp.end())
+            return dp[word];
+
+        vector<string> &words = mp[word.size() + 1];
+        int ans = 0;
+
+        for(auto w : words){
+            if(are_words_pre(word, w)){
+                ans = max(ans,kev(w, dp, mp));
+            }
+        }
+
+        dp[word] = ans + 1;
+        return dp[word];
+    }
+    int longestStrChain(vector<string>& words) {
+        map<int, vector<string>> mp;
+        unordered_map<string, int> dp;
+        
+        for(auto word : words){
+            mp[word.size()].push_back(word);
+        }
+
+        int ans = 1;
+        for(auto word : words){
+            ans = max(ans, kev(word, dp, mp));
+        }
+
+        return ans;
+    }
+};
