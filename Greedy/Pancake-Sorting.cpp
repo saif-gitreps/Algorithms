1e0061a69976fdc8f01f6a6a69e48e7f720c52e7
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-06-23 00:53:34 +0600

    Update Pancake-Sorting.cpp

--- Greedy/Pancake-Sorting.cpp
+++ Greedy/Pancake-Sorting.cpp
@@ -51,3 +51,32 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+// Apparently, this was some sorting algorithm, didnt guess the trick properly as usual.
+// our goal is to find the biggest number (and decreasing from there) and put it in its place
+// First reverse from 0 till found location (Brings that element to the front) and then reverse 
+// from 0 to the original position of that element. 
+public:
+    vector<int> pancakeSort(vector<int>& a) {
+        vector<int> ans;
+
+        for(int i = a.size(); i > 0; i--){
+            for(int j = 0; j < a.size(); j++){
+                if(a[j] == i){
+                    reverse(a.begin(), a.begin() + j + 1);
+                    ans.push_back(j + 1);
+                    // reason for a.begin() + i is because
+                    // we are putting that element in its respective place
+                    // knowing that array values are 1...N(N == a.size)
+                    reverse(a.begin(), a.begin() + i);
+                    ans.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
