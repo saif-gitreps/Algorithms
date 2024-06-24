629aea2b79b320ae17f4c2a79b7b822bac376b3a
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-06-24 23:40:33 +0600

    Update Pacific-Atlantic-Water-Flow.cpp

--- BFS-DFS/Pacific-Atlantic-Water-Flow.cpp
+++ BFS-DFS/Pacific-Atlantic-Water-Flow.cpp
@@ -148,3 +148,55 @@ public:
         return ans;
     }
 };
+
+
+// Solved after 1 week 1 Day later of Revisit
+
+class Solution {
+public:
+    vector<int> offset = {0, 1, 0, -1, 0};
+
+    bool outOfBounds(int row, int col, int m, int n){
+        return row < 0 || col < 0 || row > m - 1 || col > n -1;
+    }
+
+    void dfs(vector<vector<int>>& matrix, vector<vector<int>>& visited, int i, int j) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        visited[i][j] = true;
+        for (int k =1 ; k < offset.size(); k++){
+            int row = i + offset[k], col = j + offset[k-1] ;
+            if(!outOfBounds(row, col, m, n) && !visited[row][col] && matrix[row][col] >= matrix[i][j])
+                    dfs(matrix, visited, row, col);
+        }
+    }
+
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<vector<int>> pacific(m, vector<int> (n, 0));
+        vector<vector<int>> atlantic(m, vector<int> (n, 0));
+        vector<vector<int>> ans;
+
+        for(int i = 0; i < m; i++){
+            dfs(heights, pacific, i, 0);
+            dfs(heights, atlantic, i, n - 1);
+        }
+
+        for(int i = 0; i < n; i++){
+            dfs(heights, pacific, 0, i);
+            dfs(heights, atlantic, m - 1, i);
+        }
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(pacific[i][j] && atlantic[i][j]){
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
