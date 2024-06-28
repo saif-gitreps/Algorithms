c2e6b85b2636978c4a757acc0cffdb10d7b6f3dd
Saifur Rahman <110230611+saif-gitreps@users.noreply.github.com>
2024-06-28 19:19:57 +0600

    Update Find-Right-Interval.cpp

--- Binary-Search/Find-Right-Interval.cpp
+++ Binary-Search/Find-Right-Interval.cpp
@@ -21,3 +21,31 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+// Damn , i found the better solution on my own, even though i took like more than an hour. 
+// Could use sorting but at the same time you need a copy of some sort, better to just use map
+// knowing that map sorts the element, also we need to store the index.
+public:
+    vector<int> findRightInterval(vector<vector<int>>& a) {
+        vector<int> ans;
+        map<int, int> mp;
+
+        for(int i = 0 ; i < a.size(); i++){
+            mp[a[i][0]] = i; 
+        }
+
+        for(auto interval : a){
+            auto it = mp.lower_bound(interval[1]);
+             
+            if (it == mp.end()) {
+                ans.push_back(-1);
+            } else {
+                ans.push_back(it->second);
+            }
+        }
+
+        return ans;
+    }
+};
