202aef35dd2121772deec85f2de8f97b87e93224
Saif <110230611+saif-gitreps@users.noreply.github.com>
2024-08-02 17:00:25 +0600

    Update Knight-Dialer.cpp

--- Dynamic-Programming/Knight-Dialer.cpp
+++ Dynamic-Programming/Knight-Dialer.cpp
@@ -99,3 +99,90 @@ public:
         return ans;
     }
 };
+
+class Solution:
+// apparently this soluton works for python and java.
+    def __init__(self):
+        self.mod = int(1e9 + 7)
+        self.offset = [
+            [1, 2], [-1, 2], [1, -2], [-1, -2], [2, 1], [-2, 1], [2, -1], [-2, -1]
+        ]
+        self.dp = []
+
+    def isValid(self, i, j):
+        if i < 0 or j < 0 or j >= 3 or i >= 4 or (i == 3 and j != 1):
+            return False
+        return True
+
+    def kev(self, i, j, n):
+        if n == 1:
+            return 1
+
+        if self.dp[n][i][j] != -1:
+            return self.dp[n][i][j]
+
+        ans = 0
+
+        for o in self.offset:
+            r = i + o[0]
+            c = j + o[1]
+            if self.isValid(r, c):
+                ans = (ans + self.kev(r, c, n - 1)) % self.mod
+
+        self.dp[n][i][j] = ans
+        return ans
+        
+    def knightDialer(self, n: int) -> int:
+        ans = 0
+        self.dp = [[[-1 for _ in range(3)] for _ in range(4)] for _ in range(n + 1)]
+
+        for i in range(4):
+            for j in range(3):
+                if not ((i == 3 and j == 0) or (i == 3 and j == 2)):
+                    ans = (ans + self.kev(i, j, n)) % self.mod
+
+        return ans
+
+
+class Solution {
+/* 
+    We know that for n == 1 , its 10 anyway.
+    I mean i understood the solution , what the intuition was.
+    hers explaination vid: https://www.youtube.com/watch?v=wRF40TQUpxg
+*/
+public:
+    const int mod = 1e9 + 7;
+    int knightDialer(int n) {
+        if (n == 1) return 10;
+
+        int ans = 0;
+        vector<int> dp(10, 1);
+        unordered_map<int, vector<int>> mp;
+        mp[0] = {4, 6};
+        mp[1] = {6, 8};
+        mp[2] = {7, 9};
+        mp[3] = {4, 8};
+        mp[4] = {3, 0, 9};
+        mp[6] = {0, 1, 7};
+        mp[7] = {2, 6};
+        mp[8] = {1, 3};
+        mp[9] = {2, 4};
+
+        for (int i = 2; i <= n; i++) {
+            vector<int> temp(10, 0);
+            for (auto [key, arr] : mp) {
+                for (auto numb : arr) {
+                    temp[key] = (temp[key] + dp[numb]) % mod;
+                }
+            }
+            dp = temp;
+        }
+
+        for (auto num : dp) {
+            ans = (ans + num) % mod;
+        }
+
+        return ans;
+    }
+};
+        
