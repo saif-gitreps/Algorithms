051e9a8050ab34d76d6fb8d89e973213ffc3c9cf
Sf <110230611+saif-gitreps@users.noreply.github.com>
2024-08-15 23:06:10 +0600

    Update Left_Recursion.cpp

--- Compiler/Left_Recursion.cpp
+++ Compiler/Left_Recursion.cpp
@@ -66,3 +66,90 @@ int main() {
         isLeftRecursion(run);
     }
 }
+
+
+// Updated 
+vector<int> findLeftRecursionIndexes(string s) {
+    vector<int> recursion_indexes;
+    if (s[0] == s[3]) {
+        recursion_indexes.push_back(3);
+    }
+
+    for (int i = 3; i < s.size(); i++) {
+        if(i + 1 < s.size() && s[i] == '|' && s[0] == s[i + 1]){
+            recursion_indexes.push_back(i + 1);
+        }
+    }
+
+    return recursion_indexes;
+}
+
+vector<string> findAlphas(string s, vector<int> indexes) {
+    vector<string> alphas;
+    for (int i = 0; i < indexes.size(); i++) {
+        int k = indexes[i] + 1;
+        string alpha = "";
+        
+        while (k < s.size() && s[k] != '|') {
+            alpha += s[k];
+            k ++;
+        }
+
+        alphas.push_back(alpha);
+    }
+    return alphas;
+}
+
+vector<string> findBetas(string s, int start) {
+    while (s[start] != '|') start++;
+    
+    vector<string> betas;
+    string temp_beta = "";
+
+    while (start < s.size()) {
+        if (s[start++] == '|' && temp_beta != "") {
+            betas.push_back(temp_beta);
+            temp_beta = "";
+            continue;
+        } 
+        temp_beta += s[start];
+    }
+    betas.push_back(temp_beta);
+    return betas;
+}
+
+void findAndEliminateLeftRecursion() {
+    string s;
+    cin >> s;
+
+    vector<int> indexes = findLeftRecursionIndexes(s);
+
+    if (indexes.size() == 0) {
+        cout << "No left recursion found.\n";
+        return;
+    }
+
+    vector<string> alphas = findAlphas(s, indexes);
+    vector<string> betas = findBetas(s, indexes[indexes.size() - 1]); 
+
+    cout << "ans: \n";
+    cout << s[0] << "->" ;
+    
+    for (int i = 0; i < betas.size(); i++) {
+        cout << betas[i] << s[0] << "'" << (i + 1 < betas.size() ? "|" : "");
+    }
+
+    cout << "\n";
+    
+    cout << s[0] << "'" << "->" ; 
+    
+    for (auto a : alphas) {
+        cout << a << "" << s[0] << "'|";
+    }
+
+    cout <<"e";
+}
+
+int main() {
+    findAndEliminateLeftRecursion();
+}
