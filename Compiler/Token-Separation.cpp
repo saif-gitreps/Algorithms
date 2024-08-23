21a5ede39ba1b92f3d08a46b0747ceb5f0037fb1
Sf <110230611+saif-gitreps@users.noreply.github.com>
2024-08-23 22:58:19 +0600

    Create Token-Separation.cpp

---
+++ Compiler/Token-Separation.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isOperator(char a) {
+    if (a == '+' || a == '-' || a == '*' || a == '%' || a == '=') {
+        return true;
+    }
+    return false;
+}
+
+bool isIdentifier(char a) {
+    if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9')) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    char s[100];
+    fgets(s, sizeof(s), stdin);
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == ' ') {
+            continue;
+        }
+
+        if (isIdentifier(s[i])) {
+            char temp[100] = "";
+            int j = 0;
+
+            while (s[i] != '\0' && s[i] != ' ' && isIdentifier(s[i])) {
+                temp[j++] = s[i++];
+            }
+            printf("%s is identifier\n", temp);
+        }
+
+        if (isOperator(s[i])) {
+            printf("%c is operator\n", s[i]);
+        }
+    }
+
+    return 0;
+}
+
+/* C++ */
+
+bool isOperator(char a) {
+    if (a == '+' || a == '-' || a == '*' || a == '%' || a == '=' )
+        return true;
+    return false;
+}
+
+bool isIdentifier(char a) {
+    if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9'))
+        return true;
+    return false;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == ' ') continue;
+
+        if (isIdentifier(s[i])) {
+            string temp = "";
+
+            while (i < s.size() && s[i] != ' ' && isIdentifier(s[i])) {
+                temp += s[i];
+                i++;
+            }
+            cout << temp << " is identifier\n";
+        } 
+        if (isOperator(s[i])) {
+            cout << s[i] << " is Operator\n";
+        }
+    }
+}
