d1cb493d9f061056309f721489fe974bbde83b20
Sf <110230611+saif-gitreps@users.noreply.github.com>
2024-08-29 22:05:30 +0600

    Create Finding-First.cpp

---
+++ Compiler/Finding-First.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "Enter the number of production: \n";
+    int n; 
+    cin >> n;
+    unordered_map<string, string> start_first_map; 
+
+    // for prime use backticks, E` 
+    cout << "Enter the production(s): \n";
+    while (n--) {
+        string temp, start = "", first = "";
+        cin >> temp;
+
+        int i = 0;
+        while (i < temp.size() && temp[i] != '-') {
+            start += temp[i]; 
+            i++;
+        }
+        i += 2; // for example, E->Fi, currrent index is at '-', need to move outta dat.
+
+        if (islower(temp[i])) {
+            // case where first is 'id'.
+            while (islower(temp[i])) {
+                first += temp[i];
+                i++;
+            }
+        } else if (i + 1 < temp.size() && isupper(temp[i]) && temp[i+1] == '`'){
+            // case where first is '+' or '(' or 'a'
+            first += temp[i];
+            first += temp[i+1];
+        } else {
+            first = temp[i];
+        }
+
+        start_first_map[start] = first;
+        // prod_array.push_back()
+    }
+
+    cout << "\n";
+
+    for (auto &it : start_first_map) {
+        // checking if it is terminal. 
+        if (start_first_map.find(it.second) != start_first_map.end()) {
+            string terminal = find_terminal(it.second, start_first_map);
+
+            cout << "first(" << it.first << ")=" << terminal;
+        } else {
+            cout << "first(" << it.first << ")=" << it.second;
+        }
+        cout << "\n";
+    }
+}
