74d16f0fe87399850128ffcff58507a5a2e4ba09
Sf <110230611+saif-gitreps@users.noreply.github.com>
2024-12-03 22:36:13 +0600

    Update Coin-Change-II.cpp

--- Dynamic-Programming/Coin-Change-II.cpp
+++ Dynamic-Programming/Coin-Change-II.cpp
@@ -72,3 +72,48 @@ public:
         return dp[amount];
     }
 };
+
+
+// other attempt
+
+class Solution {
+public:
+    int kev(int i, int t, vector<int> &a, vector<vector<int>> &dp) {
+        if (i < 0 || t < 0) return 0;
+
+        if (t == 0) return 1;
+
+        if (dp[i][t] != -1) return dp[i][t];
+
+        return dp[i][t] = kev(i, t-a[i], a, dp) + kev(i-1, t, a, dp);
+    }
+    int change(int amount, vector<int>& coins) {
+        vector<vector<int>> dp(coins.size(), vector<int> (amount+1, -1));
+        return kev(coins.size()-1, amount, coins, dp);
+    }
+};
+
+// this bottom-up should work, but it dosent work for some reason, overflow error ? tf
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int mod = 1e9;
+        vector<vector<int>> dp(coins.size()+1, vector<int> (amount+1, 0));
+
+        for (int i = 0; i <= coins.size(); i++)
+            dp[i][0] = 1;
+
+        for (int i = 1; i <= coins.size(); i++) {
+            for (int j = 1; j <= amount; j++) {
+                dp[i][j] = dp[i - 1][j];
+                
+                if (j - coins[i - 1] >= 0) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp[coins.size()][amount];
+    }
+};
