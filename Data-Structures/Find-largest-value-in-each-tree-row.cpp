24f575818f297024fe4b7e408a6653a94441b11b
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-01-09 20:01:45 +0600

    Update Find-largest-value-in-each-tree-row.cpp

--- Data-Structures/Find-largest-value-in-each-tree-row.cpp
+++ Data-Structures/Find-largest-value-in-each-tree-row.cpp
@@ -59,3 +59,43 @@ public:
         return ans;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+// found a better and clever solution. Here we know that the answer variable cannot be fixed becauese we dont know the
+// the fixed number of depth that we might have to go. that solves the problem.
+public:
+    void dfs(TreeNode *root, int depth, vector<int> &ans) {
+        if (root == NULL) {
+            return;
+        }
+
+        if ( depth >= ans.size()) {
+            for (int i = ans.size(); i <= depth; i++) {
+                ans.push_back(0);
+            }
+            ans[depth] = root->val;
+        } else {
+            ans[depth] = max(root->val, ans[depth]);
+        }
+
+        dfs(root->left, depth+1, ans);
+        dfs(root->right, depth+1, ans);
+    }
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> ans;
+        dfs(root, 0, ans);
+        return ans;
+    }
+};
