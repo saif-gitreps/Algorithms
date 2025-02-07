6f471249bd15d4b719fa20e67ac115f97ec2cbae
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-02-07 23:30:51 +0600

    Update Making-A-Lage-Island.cpp

--- BFS-DFS/Making-A-Lage-Island.cpp
+++ BFS-DFS/Making-A-Lage-Island.cpp
@@ -32,3 +32,48 @@ public:
         return mx;   
     }
 };
+
+
+class Solution {
+public:
+    int N;
+    int dfs(vector<vector<int>>& grid, int x, int y, int index) {
+        if (x < 0 || x >= N || y < 0 || y >= N || grid[x][y] != 1) return 0;
+        grid[x][y] = index;
+        return 1 + dfs(grid, x + 1, y, index) + dfs(grid, x - 1, y, index) + dfs(grid, x, y + 1, index) + dfs(grid, x, y - 1, index);
+    }
+
+    int largestIsland(vector<vector<int>>& grid) {
+        N = grid.size();
+        int index = 2, res = 0;
+        unordered_map<int, int> area;
+
+        for (int x = 0; x < N; ++x) {
+            for (int y = 0; y < N; ++y) {
+                if (grid[x][y] == 1) {
+                    area[index] = dfs(grid, x, y, index);
+                    res = max(res, area[index++]);
+                }
+            }
+        }
+
+        for (int x = 0; x < N; ++x) {
+            for (int y = 0; y < N; ++y) {
+                if (grid[x][y] == 0) {
+                    unordered_set<int> seen;
+                    int cur = 1;
+                    for (auto& dir : vector<vector<int>>{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}) {
+                        int nx = x + dir[0], ny = y + dir[1];
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < N && grid[nx][ny] > 1 && !seen.count(grid[nx][ny])) {
+                            seen.insert(grid[nx][ny]);
+                            cur += area[grid[nx][ny]];
+                        }
+                    }
+                    res = max(res, cur);
+                }
+            }
+        }
+
+        return res;
+    }
+};
