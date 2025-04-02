09f2418f831472dd86b8f2c8dbcdfff1780d06cf
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-04-02 23:59:34 +0600

    Create Find-Missing-And-Repeated.cpp

---
+++ Array/Find-Missing-And-Repeated.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int mx = 0;
+        for(int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                mx = max(mx, grid[i][j]);
+            }
+        }
+        vector<int> mp(mx + 1, 0);
+        
+        for(int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                mp[grid[i][j]]++;
+            }
+        }
+        vector<int> ans(2);
+        
+        for (int i = 1; i <= mx; i++) {
+            if (mp[i] > 1) {
+                ans[0] = i;
+            }
+            if (mp[i] == 0) {
+                ans[1] = i;
+            }
+        }
+
+        return ans;
+    }
+};
