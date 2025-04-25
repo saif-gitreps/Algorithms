9d652ce72178066efdddcbf39871f2b0983f4ab6
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-04-25 23:52:50 +0600

    Update count-the-hidden-sequences.cpp

--- Array/count-the-hidden-sequences.cpp
+++ Array/count-the-hidden-sequences.cpp
@@ -7,3 +7,21 @@
         }
         return max(0L, (upper - lower) - (ma - mi) + 1);
     }
+
+
+// OJ: https://leetcode.com/contest/biweekly-contest-70/problems/count-the-hidden-sequences/
+// Author: github.com/lzl124631x
+// Time: O(N)
+// Space: O(1)
+class Solution {
+public:
+    int numberOfArrays(vector<int>& A, int lower, int upper) {
+        long sum = 0, mn = 0, mx = 0;
+        for (int n : A) {
+            sum += n;
+            mn = min(mn, sum);
+            mx = max(mx, sum);
+        }
+        return max(0L, mn - mx + upper - lower + 1);
+    }
+};
