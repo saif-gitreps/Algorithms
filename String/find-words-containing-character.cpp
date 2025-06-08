ec51c77c1899290168d431566a9f1d4ec1b35161
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-06-08 23:57:32 +0600

    Update find-words-containing-character.cpp

--- String/find-words-containing-character.cpp
+++ String/find-words-containing-character.cpp
@@ -1,11 +1,11 @@
 class Solution {
 // fails for half of the cases.
-
+// fix: minor error, most of the sol are N^2 anyway
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for (int i = 0; i < words.size(); i++) {
-            for (int j = 0; j < words[0].size(); j++) {
+            for (int j = 0; j < words[i].size(); j++) {
                 if (words[i][j] == x) {
                     ans.push_back(i);
                     break;
