f2636f1cc7a8bc535c0cbb126890e90afe8c46b5
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-06-10 23:57:09 +0600

    Update find-the-lexicographically-largest-string-from-the-box-i.cpp

--- String/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ String/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -12,3 +12,15 @@
         }
         return res;
     }
+
+
+class Solution {
+public:
+    string answerString(string word, int numFriends) {
+        if (numFriends == 1) return word;
+        string res = "";
+        for (int i = 0 ; i < word.size() ; i++) 
+            res = max(res, word.substr(i, word.length() - numFriends + 1));
+        return res;
+    }
+};
