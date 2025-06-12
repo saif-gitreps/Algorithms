c993cbc453bf1281e8d8e614017969aea86cdc11
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-06-12 20:53:30 +0600

    Update find-the-lexicographically-largest-string-from-the-box-i.cpp

--- String/find-the-lexicographically-largest-string-from-the-box-i.cpp
+++ String/find-the-lexicographically-largest-string-from-the-box-i.cpp
@@ -24,3 +24,54 @@ public:
         return res;
     }
 };
+
+
+// absolute bruteforce, try to trace using "dbca" and num = 3
+
+#include <iostream>
+#include <string>
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(a) cerr<< "line "<<__LINE__ <<" : "<< #a <<" --> "<<(a)<<"\n"
+
+string maxStr = "";
+
+void dfs(string& word, int index, int remainingCuts, vector<string>& current) {
+    int n = word.size();
+    if (remainingCuts == 0) {
+        if (index < n) {
+            current.push_back(word.substr(index));
+            for (string& s : current) {
+                debug(s);
+                if (s > maxStr) maxStr = s;
+            }
+
+            debug("--");
+            current.pop_back();
+        }
+        return;
+    }
+
+    for (int i = index; i < n - remainingCuts; ++i) {
+        string part = word.substr(index, i - index + 1);
+        current.push_back(part);
+        debug(part);
+        dfs(word, i + 1, remainingCuts - 1, current);
+        current.pop_back();
+    }
+}
+
+string answerString(string word, int numFriends) {
+    vector<string> current;
+    dfs(word, 0, numFriends - 1, current);
+    return maxStr;
+}
+    
+int main() {
+    string words = "dbca";
+    int numFriends = 3;
+
+    cout << answerString(words, numFriends);
+}
+
+
